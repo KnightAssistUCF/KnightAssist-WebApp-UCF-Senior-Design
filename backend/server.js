81c80cf0a1fd4959ad446024ef87f1aa191ccd9d
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2023-10-01 12:36:06 -0400

    added package changes, DB connects successfully, backend starts on the specified port, front end on its port as well

--- backend/server.js
+++ backend/server.js
@@ -50,10 +50,19 @@ app.use((req, res, next) =>
 
 // importing routes here soon (Endpoints)
 
+
 /*
   @yohan: if we plan to have specific settings for the configuration in production, we will need to add that here.
           can be omitted for now
 */
+if (process.env.STATUS === 'production') {
+    // Serve any static files
+    // app.use(express.static(path.join(__dirname, '../frontend/build')));
+    // Handle React routing, return all requests to React app
+    // app.get('*', function(req, res) {
+    //     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    // });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
