ae090e6a95930b77c725082880e6e8553816004e
NSel1727 <noahseligson18@gmail.com>
2023-10-11 23:01:48 -0400

    Add logout button

--- frontend/src/components/OrgPortal/Header.js
+++ frontend/src/components/OrgPortal/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PageTitle from '../PageTitle';
-
+import {LuLogOut} from 'react-icons/lu';
 function Header()
 {
     return(
@@ -12,6 +12,11 @@ function Header()
                 <a className="headerLink" href="">home</a>
                 <a className="headerLink" href="">about us</a>
                 <a className="headerLink" href="">contact</a>
+                <div className="bottom">
+                    <button className='luWrapper'>
+                        <LuLogOut className='logoutIcon' onClick={() => (console.log("log out"))}/>
+                    </button>
+                </div>
             </nav>
         </div>
      </div>
