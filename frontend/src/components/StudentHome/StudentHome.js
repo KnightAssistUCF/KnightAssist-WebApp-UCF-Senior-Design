71c91bb57428fb1cc763be1f6abd026332e3c7c2
AnishaRan <anisharanjan55@gmail.com>
2023-10-13 18:18:43 -0400

    Saving progress

--- frontend/src/components/StudentHome/StudentHome.js
+++ frontend/src/components/StudentHome/StudentHome.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button} from 'react-bootstrap';
 import Logo from '../Logo';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import './StudentHome.css';
+
 
 function StudentHome()
 {
    return(
-      <div className='titleLogo'>
-        <Logo/>
-        <h1 id="title" className='title center'>Knight Assist</h1>
+      <div className='homePage'>
+        <div class="temp-navbar"></div>
+        <div class="title">Welcome, First Last</div>
+        <div class="progress-bar" style={{ width: 460, height: 150 }}><CircularProgressbar value={86} text={`13/15`} /></div>
+        <div class="titlesub">Upcoming Volunteer Shifts</div>
+        <div class="my-table">
+          <table className="table table-hover text-nowrap">
+          <thead className="thead-dark" class="table-dark">
+            <tr>
+              <th className="col-2">Organization Name</th>
+              <th className="col-1">Date</th>
+              <th className="col-1">Duration</th>
+              <th className="col-1">Check In</th>
+              <th className="col-1">Check Out</th>
+              <th className="col-1">Cancel RSVP</th>
+            </tr>
+          </thead>
+          <tbody>
+                <tr>
+                  <td>Arboretum</td>
+                  <td>10/20/23</td>
+                  <td>2.5 hours</td>
+                  <td>12:30pm</td>
+                  <td>3:00pm</td>
+                  <td className="centered-column">
+                    <Button className="btn-danger" >Cancel RSVP</Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Knight Hacks</td>
+                  <td>10/20/23</td>
+                  <td>2.5 hours</td>
+                  <td>12:30pm</td>
+                  <td>3:00pm</td>
+                  <td className="text-center">
+                    <Button className="btn-danger" >Cancel RSVP</Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>UCF Volunteer</td>
+                  <td>10/20/23</td>
+                  <td>2.5 hours</td>
+                  <td>12:30pm</td>
+                  <td>3:00pm</td>
+                  <td>
+                    <Button className="btn-danger" >Cancel RSVP</Button>
+                  </td>
+                </tr>
+              
+            
+          </tbody>
+        </table>
+      </div>
       </div>
    );
 };
