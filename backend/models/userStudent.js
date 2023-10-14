0102e0eac4b99c0f1afaa9a3860b01fe470559b5
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2023-10-14 13:27:42 -0400

    addressed Schema Updates For Students/Users

--- backend/models/userStudent.js
+++ backend/models/userStudent.js
@@ -1,7 +1,13 @@
+const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
 
 const userStudentSchema = new mongoose.Schema({
     /* To be added maybe: graduation date, major, etc. */
+    studentID: {
+        type: String, // keeping it as string for now, maybe it makes things easier (alternative Int32)
+        required: true,
+        unique: true
+    },
     firstName: {
         type: String,
         required: true
@@ -22,6 +28,28 @@ const userStudentSchema = new mongoose.Schema({
     profilePicture: {
         type: String,
         default: 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
+    },
+    favoritedOrganizations: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'organization',
+    }],
+    eventsRSVP: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'event',
+    }],
+    eventsHistory: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'event',
+    }],
+    totalVolunteerHours: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    semesterVolunteerHourGoal: {
+        type: Number,
+        // required: true,
+        default: 0
     },
     __v: {
         type: String,
