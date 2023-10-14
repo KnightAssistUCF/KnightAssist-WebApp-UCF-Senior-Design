33d5fe7e910074f03735bd0ff4ee2a4741aa2137
NSel1727 <noahseligson18@gmail.com>
2023-10-14 17:33:53 -0400

    Begin onchange for when item is selected in search

--- frontend/src/components/OrgPortal/SearchOrg.js
+++ frontend/src/components/OrgPortal/SearchOrg.js
@@ -3,18 +3,23 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const orgs = ["Knight Hacks", "Hack@UCF", "Knights Build"]
-
 //TODO: Call API to search for organizations upon key click and set that to orgs
+const orgs = [{label: "Knight Hacks", id: "kh@example.com"}, {label: "Hack@UCF", id: "hack@example.com"}]
 
 function SearchOrg() {
+
+  function openOrgPage(email){
+    console.log(email);
+  }
+
   return (
     <div>
       <Stack className="orgSearch" spacing={2} sx={{ width: 300 }}>
-        <Autocomplete
+        <Autocomplete 
           freeSolo
           id="free-solo-2-demo"
           disableClearable
+          onChange={(e, value) => openOrgPage(value.id)}
           options={orgs}
           renderInput={(params) => (
             <TextField
