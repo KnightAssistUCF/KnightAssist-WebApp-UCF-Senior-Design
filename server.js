0bb5fcddbcfd7a83fc4fb501d5f6fc738108e040
AnishaRan <anisharanjan55@gmail.com>
2023-10-13 21:59:25 -0400

    correcting server.js routing

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const dbURL = process.env.atlasDB_LINK;
 
 
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', port);
 app.use(cors());
 app.use(bodyParser.json());
 app.disable('x-powered-by');
@@ -56,7 +56,7 @@ const organization_signup = require('./backend/routes/organizationSignUp');
 app.use('/api/organizationSignUp', organization_signup);
 
 // login supports both organization and user lookup
-const login = require('./Login'); 
+const login = require('./backend/routes/Login'); 
 app.use('/api/Login', login);
 
 const userStudentsDelete = require('./backend/routes/userStudentsDelete');
@@ -74,6 +74,7 @@ app.use('/api/searchOrganization', searchOrganization);
 const editUserProfile = require('./backend/routes/editUserProfile');
 app.use('/api/editUserProfile', editUserProfile);
 
+
 /*
   @yohan: if we plan to have specific settings for the configuration in production, we will need to add that here.
           can be omitted for now
@@ -87,7 +88,6 @@ if (process.env.STATUS === 'production') {
         res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
     });
 }
-
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
