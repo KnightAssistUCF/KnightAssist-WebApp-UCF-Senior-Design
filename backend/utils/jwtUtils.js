aaf0e3fe70a7f43c9f581d77cae5b0158ab0f5fd
Yohan (-aka Yahya) Hmaiti <88591695+YHmaiti@users.noreply.github.com>
2023-10-15 09:18:32 -0400

    improved jwtUtils

--- backend/utils/jwtUtils.js
+++ backend/utils/jwtUtils.js
@@ -1,29 +1,25 @@
-const jwt = require('jsonwebtoken'); 
+const jwt = require('jsonwebtoken');
 
-let jwtSecret = process.env.JWT_SECRET_KEY;
-let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+const jwtSecret = process.env.JWT_SECRET_KEY;
+const tokenHeaderKey = process.env.TOKEN_HEADER_KEY || 'Authorization'; // yohan: added the authorization string as default for now
 
 const generateToken = (payload, jwtSecret) => {
-    return jwt.sign(payload, jwtSecret, { expiresIn: '1800s' }); 
+    return jwt.sign(payload, jwtSecret, { expiresIn: '1800s' }); // yohan: I increased the time out
 }
 
-const validateToken = (req, res, tokenHeaderKey) => {
-
-    try {
-
-        const token = req.header(tokenHeaderKey);
-        const verified = jwt.verify(token, jwtSecret); 
-
-        if (!verified){ 
-            return res.status(401).send(error); 
-
+const authenticateToken = (req, res, next) => { // yohan: added next, to be used to move to the next route in the middleware 
+    const token = req.header(tokenHeaderKey); // [Melanie] if this doesn't work use: req.headers.authorization;
+    if (!token) {
+        return res.status(401).send("Access Denied (1st stage) - No token provided");
+    }
+    
+    jwt.verify(token, jwtSecret, (err, user) => {
+        if (err) {
+            return res.status(403).send("Access Denied (2nd stage) - Invalid token"); // stop moving on, so I addded a return 
         }
-    }
+        req.user = user; // store the current user
+        next(); // move to the next middleware route
+    });
+};
 
-    catch (error) {
-        
-        return res.status(401).send(error); 
-    }
-} 
-
-module.exports = { generateToken, validateToken };
+module.exports = { generateToken, authenticateToken };
