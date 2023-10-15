cd6999de3e50a760515f521ccce18ab3b6d9445e
Melanie Ehrlich <melanie6104@icloud.com>
2023-10-15 02:05:54 -0400

    add jwtUtils.js

---
+++ backend/utils/jwtUtils.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken'); 
+
+let jwtSecret = process.env.JWT_SECRET_KEY;
+let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+
+const generateToken = (payload, jwtSecret) => {
+    jwt.sign(payload, jwtSecret, { expiresIn: '1200s' }); 
+}
+
+const validateToken = (req, res, tokenHeaderKey) => {
+
+    const token = req.header(tokenHeaderKey);
+    const verified = jwt.verify(token, jwtSecret); 
+
+    if (!verified){ 
+        return res.status(401).send(error); 
+    }
+} 
+
+module.exports = { generateToken, validateToken };
