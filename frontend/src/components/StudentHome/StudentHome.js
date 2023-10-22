247829568f0bfee4d20924624bcf9940de232bc9
AnishaRan <anisharanjan55@gmail.com>
2023-10-22 19:01:03 -0400

    styling and calendar component is buggy

--- frontend/src/components/StudentHome/StudentHome.js
+++ frontend/src/components/StudentHome/StudentHome.js
@@ -32,15 +32,27 @@ function StudentHome()
     setVolunteerGoal(e.target.value);
   };
 
-  const currentDate = '2018-11-01';
+  const currentDate = '2023-10-21';
   const schedulerData = [
-  { startDate: '2023-11-01T09:45', endDate: '2023-11-01T11:00', title: 'Meeting' },
-  { startDate: '2023-11-01T12:00', endDate: '2023-11-01T13:30', title: 'Go to a gym' },
+    { startDate: '2023-10-19T09:45', endDate: '2023-10-19T11:00', title: 'Arboretum', color: '#4B0082' },
+    { startDate: '2023-10-20T12:00', endDate: '2023-10-20T13:30', title: 'Knight Hacks', color: '#4B0082' },
 ];
+const Appointment = ({ children, style, data, ...restProps }) => (
+  <Appointments.Appointment
+    {...restProps}
+    style={{
+      ...style,
+      backgroundColor: data.color
+    }}
+  >
+    {children}
+  </Appointments.Appointment>
+);
 
 const handleCurrentViewNameChange = (newViewName) => {
   setCurrentViewName(newViewName);
 };  
+
 
   
 
@@ -49,96 +61,99 @@ const handleCurrentViewNameChange = (newViewName) => {
     
       <div id='homePage'>
         <div class="StudentHomePage-title">Welcome, First Last</div>
-        <div className="calendar">
-          <Paper>
-            <Scheduler
-              data={schedulerData}
-              height={660}
-            >
-              <ViewState
-                defaultCurrentDate={currentDate}
-                currentViewName={currentViewName}
-                onCurrentViewNameChange={handleCurrentViewNameChange}
-              />
-              <MonthView name="month-view" displayName="Month" />
-              <WeekView
-                startDayHour={10}
-                endDayHour={19}
-              />
-              <DayView />
-              <Toolbar />
-              <ViewSwitcher />
-              <Appointments />
-            </Scheduler>
-          </Paper>
-        </div>
-        <div class="content-container">
+        <div class="StudentHomePage-subtitle">Fall 2023</div>
         
-          <div class="progress-bar" style={{ width: 460, height: 150 }}><CircularProgressbar value={(13.0/volunteerGoal)*100} text={'13/'+volunteerGoal} /></div>
+        <div className="content-container">
+          <div className="calendar">
+            <Paper>
+              <Scheduler id="my-calendar"
+                timeZone="America/New_York"
+                data={schedulerData}
+                // height={660}
+              >
+                <ViewState
+                  defaultCurrentDate={currentDate}
+                  currentViewName={currentViewName}
+                  onCurrentViewNameChange={handleCurrentViewNameChange}
+                />
+                <MonthView name="month-view" displayName="Month" />
+                <WeekView
+                  startDayHour={8}
+                  endDayHour={19}
+                />
+                <Toolbar />
+                <ViewSwitcher />
+                <Appointments id="appointment" appointmentComponent={Appointment} />
+              </Scheduler>
+            </Paper>
+          </div>
           <div class="StudentHomePage-card">
-            <p><strong>Fall 2023</strong></p>
-
+            <p class="upcoming-shift">Next Shift: <strong>Arboretum</strong></p>
+            <p class="upcoming-shift-time">October 20th, 12:30pm-3pm</p>
+            <div class="progress-bar" style={{ height: 110 }}><CircularProgressbar value={(13.0/volunteerGoal)*100} text={'13/'+volunteerGoal} /></div>
             <div class="volunteer-goal">
               <label for="volunteerGoal">Volunteer Hour Goal:</label>
               <input
-                type="number"
+                // type="number"
                 id="volunteerGoal"
                 value={volunteerGoal}
                 onChange={handleVolunteerGoalChange}
               />
             </div>
 
-            <p class="upcoming-shift">Upcoming Shift: <strong>Arboretum</strong></p>
-            <p class="upcoming-shift-time">October 20th, 12:30pm-3pm</p>
-            
           </div>
         </div>
-        <div class="titlesub">Upcoming Volunteer Shifts</div>
-        <div class="my-table table-responsive">
-          <table className="table table-hover text-nowrap">
-          <thead className="thead-dark" class="table-dark">
-            <tr>
-              <th className="col-2">Organization Name</th>
-              <th className="col-1">Date</th>
-              <th className="col-1">Duration</th>
-              <th className="col-1">Check In</th>
-              <th className="col-1">Check Out</th>
-              <th className="col-1">Cancel RSVP</th>
-            </tr>
-          </thead>
-          <tbody>
-                <tr>
-                  <td>Arboretum</td>
-                  <td>10/20/23</td>
-                  <td>2.5 hours</td>
-                  <td>12:30pm</td>
-                  <td>3:00pm</td>
-                  <td className="text-center">
-                    <Button className="cancelButton" variant="outlined" color="error" onClick={() => setModalOpen(true)}>Cancel RSVP</Button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Knight Hacks</td>
-                  <td>10/20/23</td>
-                  <td>2.5 hours</td>
-                  <td>12:30pm</td>
-                  <td>3:00pm</td>
-                  <td className="text-center">
-                    <Button className="cancelButton" variant="outlined" color="error" onClick={() => setModalOpen(true)} >Cancel RSVP</Button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>UCF Volunteer</td>
-                  <td>10/20/23</td>
-                  <td>2.5 hours</td>
-                  <td>12:30pm</td>
-                  <td>3:00pm</td>
-                  <td>
-                    <Button className="cancelButton" variant="outlined" color="error" onClick={() => setModalOpen(true)}>Cancel RSVP</Button>
-                  </td>
-                </tr>
-          </tbody>
-        </table>
+
+        <div className="table-group">
+
+        
+          <div class="titlesub">Upcoming Volunteer Shifts</div>
+          <div class="my-table table-responsive">
+            <table className="table table-hover text-nowrap">
+            <thead className="thead-dark" class="table-bordered table-light">
+              <tr>
+                <th className="col-2">Organization Name</th>
+                <th className="col-1">Date</th>
+                <th className="col-1">Duration</th>
+                <th className="col-1">Check In</th>
+                <th className="col-1">Check Out</th>
+                <th className="col-1">Cancel RSVP</th>
+              </tr>
+            </thead>
+            <tbody>
+                  <tr>
+                    <td>Arboretum</td>
+                    <td>10/20/23</td>
+                    <td>2.5 hours</td>
+                    <td>12:30pm</td>
+                    <td>3:00pm</td>
+                    <td className="text-center">
+                      <Button className="cancelButton" variant="contained" color="error" onClick={() => setModalOpen(true)}>Cancel</Button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Knight Hacks</td>
+                    <td>10/20/23</td>
+                    <td>2.5 hours</td>
+                    <td>12:30pm</td>
+                    <td>3:00pm</td>
+                    <td className="text-center">
+                      <Button className="cancelButton" variant="contained" color="error" onClick={() => setModalOpen(true)} >Cancel</Button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>UCF Volunteer</td>
+                    <td>10/20/23</td>
+                    <td>2.5 hours</td>
+                    <td>12:30pm</td>
+                    <td>3:00pm</td>
+                    <td>
+                      <Button className="cancelButton" variant="contained" color="error" onClick={() => setModalOpen(true)}>Cancel</Button>
+                    </td>
+                  </tr>
+            </tbody>
+          </table>
+      </div>
       </div>
 
         <Dialog open={open} onClose={() => setModalOpen(false)}>
