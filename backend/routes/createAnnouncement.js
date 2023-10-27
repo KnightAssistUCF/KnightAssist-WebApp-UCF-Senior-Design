edc0879074da9c1834309c53b90f35682b528ece
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2023-10-27 14:07:24 -0400

    added edge case handling for the add announcement endpoint and new response statuses

--- backend/routes/createAnnouncement.js
+++ backend/routes/createAnnouncement.js
@@ -3,21 +3,28 @@ const router = express.Router();
 const orgDB = require("../models/organization");
 
 router.post('/', async (req, res) => {
-    const organizationID = req.body.organizationID;
+    const fetchID = req.body.organizationID;
+    const newAnnouncementContent = req.body.content;
+    const newAnnouncementTitle = req.body.title;
 
+    if (!fetchID) return res.status(400).send('Organization ID not provided');
+    if (!newAnnouncementContent) return res.status(400).send('Announcement content is needed');
+    if (!newAnnouncementTitle) 
+        newAnnouncementTitle = req.body.title || 'Untitled';
     try {
-        const organization = await orgDB.findOne({ organizationID });
+        const organization = await orgDB.findOne({ organizationID: fetchID });
         if (!organization) return res.status(404).send('Organization not found');
         
         var newAnnouncement =  {
             "date": Date.now(),
-            "title": req.body.title,
-            "content": req.body.content
+            "title": newAnnouncementTitle,
+            "content": newAnnouncementContent
         }
 
         organization.updates.push(newAnnouncement);
         await organization.save();
-        console.log(organization.updates);
+        // print the latest added announcement
+        console.log(organization.updates[organization.updates.length - 1]);
         res.status(200).send('Announcement created successfully');
     } catch (error) {
         console.error(error);
