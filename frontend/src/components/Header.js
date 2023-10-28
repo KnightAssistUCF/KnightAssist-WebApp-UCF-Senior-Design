836175a79f88b54c621af7f3f893f992f13b92c4
AnishaRan <anisharanjan55@gmail.com>
2023-10-28 00:40:40 -0400

    sidebar expanded fixed, removing tooltips when sidebar is expanded

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageTitle from './PageTitle';
 import {BiMenu, BiHome, BiSearch, BiBell, BiHistory, BiCog, BiLogOut} from 'react-icons/bi';
 import './Header.css';
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import { styled } from '@mui/material/styles';
-import Zoom from '@mui/material/Zoom';
 import Button from '@mui/material/Button';
 import { purple } from '@mui/material/colors';
 import IconButton from '@mui/material/IconButton';
@@ -24,38 +23,46 @@ function Header()
         },
       }));
 
-      //customizing log out button
+      //customizing big log out button
       const LogOutButton = styled(Button)(({ theme }) => ({
         color: theme.palette.getContrastText(purple[500]),
-        background: 'linear-gradient(45deg, #5e548b 30%, #4f457a 90%)',
+        background: 'linear-gradient(45deg, #00785A 30%, #00785A 90%)',
         '&:hover': {
-          background: 'linear-gradient(45deg, #7266a9 30%, #4f457a 90%)',
+          background: 'linear-gradient(45deg, #008463 30%, #008463 80%)',
         },
       }));
+      //customizing small log out button
       const SmallLogOutButton = styled(IconButton)(({ theme }) => ({
         color: theme.palette.getContrastText(purple[500]),
-        background: 'linear-gradient(45deg, #5e548b 30%, #4f457a 90%)',
+        background: 'linear-gradient(45deg, #00785A 30%, #00785A 90%)',
         '&:hover': {
-          background: 'linear-gradient(45deg, #7266a9 30%, #4f457a 90%)',
+          background: 'linear-gradient(45deg, #008463 30%, #008463 90%)',
         },
       }));
+
+      let menuButton = document.querySelector(".menuIcon");
+      let sideBarAction = document.querySelector(".sidebar");
+      const [isSidebarActive, setSidebarActive] = useState(false);
+      const handleToggleSidebar = () => {
+        setSidebarActive(!isSidebarActive);
+      };
 
       
 
     return(
      <div>
         
-        <div className="sidebar">
+        <div className={`sidebar ${isSidebarActive ? 'active' : ''}`}>
             <div className="logo_content">
                 <div className="logo">
                     {/* logo image here */}
                     <div className="logo_name">KnightAssist</div>
                 </div>
-                <BiMenu className='menuIcon'></BiMenu>
+                <BiMenu onClick={handleToggleSidebar} className='menuIcon'></BiMenu>
             </div>
             <ul className="nav_list">
                 <li>
-                    <LightTooltip title="Home" placement="right" className="custom-tooltip">
+                    <LightTooltip title={!isSidebarActive ? "Home" : ""} placement="right" className="custom-tooltip">
                         <a href="#">
                             <BiHome className='homeIcon'></BiHome>
                             <span class="links_name">Home</span>
@@ -63,7 +70,7 @@ function Header()
                     </LightTooltip>
                 </li>
                 <li>
-                    <LightTooltip title="Events" placement="right" className="custom-tooltip">
+                    <LightTooltip title={!isSidebarActive ? "Events" : ""} placement="right" className="custom-tooltip">
                         <a href="#">
                             <BiSearch className='searchIcon'></BiSearch>
                             <span class="links_name">Events</span>
@@ -71,15 +78,15 @@ function Header()
                     </LightTooltip>
                 </li>
                 <li>
-                    <LightTooltip title="Notifications" placement="right" className="custom-tooltip">
+                    <LightTooltip title={!isSidebarActive ? "Notifications" : ""} placement="right" className="custom-tooltip">
                         <a href="#">
                             <BiBell className='notificationIcon'></BiBell>
-                            <span class="links_name">Notifications</span>
+                            <span class="links_name">Alerts</span>
                         </a>
                     </LightTooltip>
                 </li>
                 <li>
-                    <LightTooltip title="History" placement="right" className="custom-tooltip">
+                    <LightTooltip title={!isSidebarActive ? "History" : ""} placement="right" className="custom-tooltip">
                         <a href="#">
                             <BiHistory className='historyIcon'></BiHistory>
                             <span class="links_name">History</span>
@@ -87,7 +94,7 @@ function Header()
                     </LightTooltip>
                 </li>
                 <li>
-                    <LightTooltip title="Settings" placement="right" className="custom-tooltip">
+                    <LightTooltip title={!isSidebarActive ? "Settings" : ""} placement="right" className="custom-tooltip">
                         <a href="#">
                             <BiCog className='settingsIcon'></BiCog>
                             <span class="links_name">Settings</span>
@@ -97,11 +104,15 @@ function Header()
             </ul>
             <div className="profile_content">
                 <div className="profile">
-                    <LogOutButton className="bigLogOut" endIcon={<BiLogOut />}>
-                        Log Out
-                    </LogOutButton>
-                    <SmallLogOutButton className="smallLogOut" style={{borderRadius: 5}} ><BiLogOut className="sLogOutIcon" />
-                    </SmallLogOutButton>
+                    <LightTooltip title={!isSidebarActive ? "Log Out" : ""} placement="right" className="custom-tooltip">
+                        <a href="#">
+                            <LogOutButton className="bigLogOut" endIcon={<BiLogOut />}>
+                                Sign Out
+                            </LogOutButton>
+                            <SmallLogOutButton className="smallLogOut" style={{borderRadius: 5}} ><BiLogOut className="smallLogOutIcon" />
+                            </SmallLogOutButton>
+                        </a>
+                    </LightTooltip>
                 </div>
             </div>
         </div>
