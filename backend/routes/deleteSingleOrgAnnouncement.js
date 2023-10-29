f33335909acb1860e3cd11d73da5f95913cdabb6
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2023-10-28 20:01:25 -0400

    fixed delete single announcement (tested)

--- backend/routes/deleteSingleOrgAnnouncement.js
+++ backend/routes/deleteSingleOrgAnnouncement.js
@@ -4,21 +4,30 @@ const router = express.Router();
 const orgDB = require("../models/organization");
 
 router.delete('/', async (req, res) => {
-    const titleToLookFor = req.body.titleToLookFor;
+    const titleToLookFor = req.body.title;
     const organizationID = req.body.organizationID;
     try {
 
         const organization = await orgDB.findOne({ organizationID: organizationID });
         if (!organization) return res.status(404).send('Organization not found in the database');
 
-        const updateIndex = organization.updates.findIndex(update => update.title === titleToLookFor);
-        if (updateIndex === -1) return res.status(404).send('Annoucement/update not found');
+        var index = -1;
+        for (var i = 0; i < organization.updates.length; i++) {
+            // comapre the string of the title to look for with the title of the announcement
+            if (organization.updates[i].title.localeCompare(titleToLookFor) === 0) {
+                console.log("Found the announcement to delete");
+                index = i;
+                break;
+            }
+        }
 
-        organization.updates.splice(updateIndex, 1);
-
-        await organization.save();
-
-        res.send('announcement/update deleted successfully');
+        if (index === -1) return res.status(404).send('Announcement not found');
+        else {
+            organization.updates.splice(index, 1);
+            const result = await organization.save();
+            
+        }
+        
     } catch (error) {
         console.error(error);
         res.status(500).send('An error occurred could not delete the announcement/update');
