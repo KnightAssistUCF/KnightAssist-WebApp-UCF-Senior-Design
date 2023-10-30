04865175e889bf6f85fad2b0460f16d4dc1c687a
AnishaRan <anisharanjan55@gmail.com>
2023-10-29 20:37:06 -0400

    rerouting and adding rough mockup of student homepage

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,129 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PageTitle from './PageTitle';
-import {BiMenu, BiHome, BiSearch, BiBell, BiHistory, BiCog, BiLogOut} from 'react-icons/bi';
+import {LuLogOut} from 'react-icons/lu';
 import './Header.css';
-import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import { purple } from '@mui/material/colors';
-import IconButton from '@mui/material/IconButton';
-import { useNavigate } from 'react-router-dom'; 
 
 function Header()
 {
-    let user = JSON.parse(localStorage.getItem('user-info'));
-    const navigate = useNavigate();
-    console.warn(user);
-
-    function logOut() {
-        // handle logOut logic
-        // bring user to landing page
-        localStorage.clear();
-        navigate('/'); 
-        
-    }
-
-    // customizing tooltip appearance
-    const LightTooltip = styled(({ className, ...props }) => (
-        <Tooltip {...props} classes={{ popper: className }} />
-      ))(({ theme }) => ({
-        [`& .${tooltipClasses.tooltip}`]: {
-          backgroundColor: theme.palette.common.white,
-          color: 'rgba(0, 0, 0, 0.87)',
-          fontSize: 15,
-          border: '1px solid #8C8C8C',
-        },
-      }));
-
-      //customizing big log out button
-      const LogOutButton = styled(Button)(({ theme }) => ({
-        color: theme.palette.getContrastText(purple[500]),
-        background: 'linear-gradient(45deg, #00785A 30%, #00785A 90%)',
-        '&:hover': {
-          background: 'linear-gradient(45deg, #008463 30%, #008463 80%)',
-        },
-      }));
-      //customizing small log out button
-      const SmallLogOutButton = styled(IconButton)(({ theme }) => ({
-        color: theme.palette.getContrastText(purple[500]),
-        background: 'linear-gradient(45deg, #00785A 30%, #00785A 90%)',
-        '&:hover': {
-          background: 'linear-gradient(45deg, #008463 30%, #008463 90%)',
-        },
-      }));
-
-      const [isSidebarActive, setSidebarActive] = useState(false);
-      const handleToggleSidebar = () => {
-        setSidebarActive(!isSidebarActive);
-      };
-
-      
-
     return(
      <div>
-        
-        <div className={`sidebar ${isSidebarActive ? 'active' : ''}`}>
-            <div className="logo_content">
-                <div className="logo">
-                    {/* logo image here */}
-                    <div className="logo_name">KnightAssist</div>
+        <div className="orgNav">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <PageTitle mainStyle="headerTitleLogo" logoStyle="logoHeader" titleStyle="titleHeader"/>
+                <a className="headerLink" href="">profile</a>
+                <a className="headerLink" href="">calendar</a>
+                <a className="headerLink" href="">about us</a>
+                <div className="bottom">
+                    <button className='luWrapper'>
+                        <LuLogOut className='logoutIcon' onClick={() => (console.log("log out"))}/>
+                    </button>
                 </div>
-                <BiMenu onClick={handleToggleSidebar} className='menuIcon'></BiMenu>
-            </div>
-            <ul className="nav_list">
-                <li>
-                    <LightTooltip title={!isSidebarActive ? "Home" : ""} placement="right" className="custom-tooltip">
-                        <a href="#">
-                            <BiHome className='homeIcon'></BiHome>
-                            <span class="links_name">Home</span>
-                        </a>
-                    </LightTooltip>
-                </li>
-                <li>
-                    <LightTooltip title={!isSidebarActive ? "Events" : ""} placement="right" className="custom-tooltip">
-                        <a href="#">
-                            <BiSearch className='searchIcon'></BiSearch>
-                            <span class="links_name">Events</span>
-                        </a>
-                    </LightTooltip>
-                </li>
-                <li>
-                    <LightTooltip title={!isSidebarActive ? "Notifications" : ""} placement="right" className="custom-tooltip">
-                        <a href="#">
-                            <BiBell className='notificationIcon'></BiBell>
-                            <span class="links_name">Alerts</span>
-                        </a>
-                    </LightTooltip>
-                </li>
-                <li>
-                    <LightTooltip title={!isSidebarActive ? "History" : ""} placement="right" className="custom-tooltip">
-                        <a href="#">
-                            <BiHistory className='historyIcon'></BiHistory>
-                            <span class="links_name">History</span>
-                        </a>
-                    </LightTooltip>
-                </li>
-                <li>
-                    <LightTooltip title={!isSidebarActive ? "Settings" : ""} placement="right" className="custom-tooltip">
-                        <a href="#">
-                            <BiCog className='settingsIcon'></BiCog>
-                            <span class="links_name">Settings</span>
-                        </a>
-                    </LightTooltip>
-                </li>
-            </ul>
-            <div className="profile_content">
-                <div className="profile">
-                    <LightTooltip title={!isSidebarActive ? "Log Out" : ""} placement="right" className="custom-tooltip">
-                            <LogOutButton className="bigLogOut" endIcon={<BiLogOut />} onClick={logOut}>
-                                Sign Out
-                            </LogOutButton>
-                            <SmallLogOutButton className="smallLogOut" style={{borderRadius: 5}} onClick={logOut}>
-                                <BiLogOut className="smallLogOutIcon" />
-                            </SmallLogOutButton>
-                    </LightTooltip>
-                </div>
-            </div>
+            </nav>
         </div>
      </div>
     );
---
+++ frontend/src/pages/StudentHomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import StudentHome from '../components/StudentHome/StudentHome';
+
+const StudentHomePage = () =>
+{
+    return(
+      <div className="studenthomepage">
+        <StudentHome />
+      </div>
+    );
+};
+
+export default StudentHomePage;
