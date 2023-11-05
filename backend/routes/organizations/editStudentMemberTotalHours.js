ebb1af4f601b0a4c4e0f6b85775a563eeb86e51d
Yohan (-aka Yahya) Hmaiti <88591695+YHmaiti@users.noreply.github.com>
2023-11-05 11:11:48 -0500

    Update editStudentMemberTotalHours.js

--- backend/routes/organizations/editStudentMemberTotalHours.js
+++ backend/routes/organizations/editStudentMemberTotalHours.js
@@ -16,7 +16,7 @@ router.post('/', authenticateToken_User, async (req, res) => {
         if (req.body.email) {
                 query.email = req.body.email;
         } else if (req.body.name) {
-                query.name = req.body.name;
+                query.userID = req.body.userID;
         } else {
                 return res.status(400).send("Search for student failed - no email or name provided");
         }
