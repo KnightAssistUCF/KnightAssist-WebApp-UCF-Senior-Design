af41e510b85352f05bbc871708f29da4103184b9
AnishaRan <anisharanjan55@gmail.com>
2023-11-13 19:31:24 -0500

    FIXED dependency problem, added top navbar

--- frontend/src/components/AboutUs/AboutUs.js
+++ frontend/src/components/AboutUs/AboutUs.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { Grid, Typography, Button, Box } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+// import Lottie, {LottieRefCurrentProps} from "lottie-react";
+// import animationData from './landingAnimation2';
+// import useStyles from '../PreLoginStyles';
 import './AboutUs.css';
+import PreLoginNavBar from '../PreLoginNavBar';
 
 function AboutUs()
 {
+	// const classes = useStyles();
+	// const theme = createTheme({
+
+	// });
+
+
     return(
       <div>
-<h1>Hello World</h1>
+		<PreLoginNavBar />
+        <h1>Hello World</h1>
       </div>
     );
 
