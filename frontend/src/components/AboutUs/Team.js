1b8bcc94cc6840d182b1e1755a3487eb8c11d0ab
AnishaRan <anisharanjan55@gmail.com>
2023-11-18 21:16:31 -0500

    spacing

--- frontend/src/components/AboutUs/Team.js
+++ frontend/src/components/AboutUs/Team.js
@@ -16,35 +16,35 @@ const Team = () => {
     <div>
         <div className="teamContainer">
             <div className="teamTitle">Meet The Team</div>
-                <Grid container spacing={2} justifyContent="center">
-                    {teamMembers.map((member, index) => (
-                    <Grid item key={index} xs={12} sm={6} md={4} lg={4}>
-                        <Card variant="none" sx={{backgroundColor:'#F5D6BA'}}>
-                        <CardMedia
-                            component="img"
-                            sx={{
-                            width: 100,
-                            height: 100,
-                            borderRadius: '50%',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            margin: 'auto',
-                            }}
-                            image={member.image}
-                        />
-                        <CardContent>
-                            <div className='teamName'>
-                            {member.name}
-                            </div>
-                            <div className='teamRole'>
-                            {member.role}
-                            </div>
-                        </CardContent>
-                        </Card>
-                    </Grid>
-                    ))}
+            <Grid container spacing={2} justifyContent="center">
+                {teamMembers.map((member, index) => (
+                <Grid item key={index} xs={12} sm={6} md={4} lg={4}>
+                    <Card variant="none" sx={{backgroundColor:'#F5D6BA'}}>
+                    <CardMedia
+                        component="img"
+                        sx={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: '50%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        margin: 'auto',
+                        }}
+                        image={member.image}
+                    />
+                    <CardContent>
+                        <div className='teamName'>
+                        {member.name}
+                        </div>
+                        <div className='teamRole'>
+                        {member.role}
+                        </div>
+                    </CardContent>
+                    </Card>
                 </Grid>
-            </div>
+                ))}
+            </Grid>
+        </div>
     </div>
   );
 };
