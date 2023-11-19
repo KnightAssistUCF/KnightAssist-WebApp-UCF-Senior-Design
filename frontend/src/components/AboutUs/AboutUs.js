10562e9acc5d77f6a76b7c4b5d6c7091f5309e7a
AnishaRan <anisharanjan55@gmail.com>
2023-11-18 19:45:01 -0500

    adding sections, adding Team component

--- frontend/src/components/AboutUs/AboutUs.js
+++ frontend/src/components/AboutUs/AboutUs.js
@@ -7,6 +7,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 // import useStyles from '../PreLoginStyles';
 import './AboutUs.css';
 import PreLoginNavBar from '../PreLoginNavBar';
+import Team from './Team';
 
 function AboutUs()
 {
@@ -18,8 +19,20 @@ function AboutUs()
 
     return(
       <div>
-		<PreLoginNavBar />
-        <h1>Hello World</h1>
+		    <PreLoginNavBar />
+        <div className="aboutContainer">
+          <div className='aboutTitle'>About Us</div>
+          <div className="aboutTopRow">
+            <div className="lottieImage">
+lottieimage!
+            </div>
+            <div className="aboutParagraph">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nostrum fugiat sed voluptatem nulla earum, fugit aspernatur. Delectus quos amet eum quidem eveniet dolore dolores repellat sit nisi aperiam? Nobis eligendi ullam ex natus quibusdam deleniti quaerat corrupti ab aspernatur voluptatum, nam beatae dolore similique numquam distinctio iste fugiat id culpa sed ea aliquid architecto. Deserunt possimus natus impedit adipisci asperiores consectetur fugit iusto, sunt quo officia! Cupiditate pariatur natus libero?
+            </div>
+          </div>
+        </div>
+        <Team></Team>
+        
       </div>
     );
 
---
+++ frontend/src/components/AboutUs/Team.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function Team()
+{
+
+    return(
+      <div>
+        Meet The Team
+        
+      </div>
+    );
+
+};
+
+export default Team;
