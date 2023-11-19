bd62d17455c3b1fb98ba87c1589d7aa7a594b878
AnishaRan <anisharanjan55@gmail.com>
2023-11-18 20:49:01 -0500

    set up team member cards

--- frontend/src/components/AboutUs/AboutUs.js
+++ frontend/src/components/AboutUs/AboutUs.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material';
-import { Grid, Typography, Button, Box } from '@mui/material';
+import { Grid, Typography, Button, Box, Card, CardMedia, CardContent } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 // import Lottie, {LottieRefCurrentProps} from "lottie-react";
 // import animationData from './landingAnimation2';
@@ -24,14 +24,14 @@ function AboutUs()
           <div className='aboutTitle'>About Us</div>
           <div className="aboutTopRow">
             <div className="lottieImage">
-lottieimage!
+              lottieimage!
             </div>
             <div className="aboutParagraph">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nostrum fugiat sed voluptatem nulla earum, fugit aspernatur. Delectus quos amet eum quidem eveniet dolore dolores repellat sit nisi aperiam? Nobis eligendi ullam ex natus quibusdam deleniti quaerat corrupti ab aspernatur voluptatum, nam beatae dolore similique numquam distinctio iste fugiat id culpa sed ea aliquid architecto. Deserunt possimus natus impedit adipisci asperiores consectetur fugit iusto, sunt quo officia! Cupiditate pariatur natus libero?
             </div>
           </div>
+          <Team></Team>
         </div>
-        <Team></Team>
         
       </div>
     );
--- frontend/src/components/AboutUs/Team.js
+++ frontend/src/components/AboutUs/Team.js
@@ -1,15 +1,52 @@
-import * as React from 'react';
+import React from 'react';
+import { Grid, Typography, Card, CardContent, CardMedia } from '@mui/material';
+import './Team.css';
 
-function Team()
-{
+const Team = () => {
+  // Sample data for demonstration purposes
+  const teamMembers = [
+    { name: 'First Last 1', role: 'Team Role 1', image: require('../Login/loginPic.png') },
+    { name: 'First Last 2', role: 'Team Role 2', image: require('../Login/loginPic.png') },
+    { name: 'First Last 3', role: 'Team Role 3', image: require('../Login/loginPic.png') },
+    { name: 'First Last 3', role: 'Team Role 3', image: require('../Login/loginPic.png') },
+    { name: 'First Last 3', role: 'Team Role 3', image: require('../Login/loginPic.png') },
+    { name: 'First Last 3', role: 'Team Role 3', image: require('../Login/loginPic.png') },
+    // Add more team members as needed
+  ];
 
-    return(
-      <div>
-        Meet The Team
-        
-      </div>
-    );
-
+  return (
+    <div>
+        <div className="teamTitle">Meet The Team</div>
+      <Grid container spacing={2} justifyContent="center">
+        {teamMembers.map((member, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4} lg={4}>
+            <Card variant="none">
+              <CardMedia
+                component="img"
+                sx={{
+                  width: 100,
+                  height: 100,
+                  borderRadius: '50%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  margin: 'auto',
+                }}
+                image={member.image}
+              />
+              <CardContent>
+                <div className='teamName'>
+                  {member.name}
+                </div>
+                <div className='teamRole'>
+                  {member.role}
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default Team;
