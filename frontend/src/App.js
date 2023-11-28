7466d37a68cda39cacfe4d2170f1bb31681d5585
Alyssa <yeekeea@gmail.com>
2023-11-27 20:30:53 -0500

    skeleton of the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import StudentHomePage from './pages/StudentHomePage';
 import StudentExplorePage from './pages/StudentExplorePage';
 import EmailVerifiedPage from './pages/EmailVerifiedPage';
 import StudentProfile from './components/StudentProfile/StudentProfile';
+import RegisterPage from './pages/RegisterPage';
 
 function App() 
 {
@@ -40,6 +41,9 @@ function App()
           </Routes>
           <Routes>
             <Route path="/emailverified" element={<EmailVerifiedPage/>}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/register" element={<RegisterPage/>}></Route>
           </Routes>
        </Router>
       </header>
---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from '../components/Register/Register';
+
+const RegisterPage = () => {
+  return(
+    <Register/>
+  );
+};
+
+export default RegisterPage
