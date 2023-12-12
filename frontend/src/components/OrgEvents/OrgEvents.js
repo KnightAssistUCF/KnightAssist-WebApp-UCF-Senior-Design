233d2436999e1357bf77e725016c4c9befed17b5
NSel1727 <noahseligson18@gmail.com>
2023-12-12 18:26:20 -0500

    Get first version of qr code scanning frontend

--- frontend/src/components/OrgEvents/OrgEvents.js
+++ frontend/src/components/OrgEvents/OrgEvents.js
@@ -8,6 +8,7 @@ import EventModal from './EventModal';
 import SearchSwitch from './SearchSwitch';
 import Header from './Header';
 import './OrgEvents.css';
+import CheckIn from '../QRCode/CheckIn';
 
 function OrgPortal()
 {
@@ -45,6 +46,7 @@ function OrgPortal()
           <EventModal setReset={setResetUpcoming} reset={resetUpcoming} setResetPast={setResetPast} resetPast={resetPast} resetSearch={resetSearch} setResetSearch={setResetSearch} eventID={eventID} open={openEvent} setOpen={setOpenEvent} setOpenAdd={setOpenModal} editMode={editMode} setEditMode={setEditMode}/>
           <UpcomingEvents setEventID={setEventID} setOpenEvent={setOpenEvent} reset={resetUpcoming}/>
           <PastEvents setEventID={setEventID} setOpenEvent={setOpenEvent} reset={resetPast}/>
+          <CheckIn/>
         </div>
       </div>
     );
