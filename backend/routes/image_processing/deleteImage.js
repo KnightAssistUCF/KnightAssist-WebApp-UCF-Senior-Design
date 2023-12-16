670de61519d1c70225cf322fcea97247154a0f9d
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2023-12-16 15:11:58 -0500

    initial batch of image processing endpoints

---
+++ backend/routes/image_processing/deleteImage.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const multer = require('multer');
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+const dotenv = require('dotenv');
+
+const Event = require('../../models/events');
+const Organization = require('../../models/organization');
+const UserStudent = require('../../models/userStudent');
+
+
+router.delete('/', async (req, res) => {
+        try {
+                const entityType = req.body.entityType;
+                const id = req.body.id;
+
+                const user = null;
+                switch (entityType) {
+                        case 'event':
+                                user = await Event.findById(id);
+                                break;
+                        case 'organization':
+                                user = await Organization.findById(id);
+                                break;
+                        case 'student':
+                                user = await UserStudent.findById(id);
+                                break;
+                        default:
+                                return res.status(400).send('Invalid entity type');
+                }
+
+                if (!user || !user.profilePicPath) {
+                        return res.status(404).send('No profile picture to delete or the user does not exist');
+                }
+
+                const filePath = user.profilePicPath;
+
+                // Delete the file from the filesystem
+                fs.unlink(filePath, (err) => {
+                        if (err) {
+                                console.error(err);
+                                return res.status(500).send('Error deleting the file');
+                        }
+
+                        user.profilePicPath = "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y";
+                        user.save()
+                                .then(() => res.json({ message: 'Profile picture deleted successfully' }))
+                                .catch((error) => {
+                                        console.error(error);
+                                        res.status(500).send('Error updating the entity');
+                                });
+                });
+
+        } catch (error) {
+                console.error(error);
+                res.status(500).send('An error occurred');
+        }
+});
+
+module.exports = router;
---
+++ backend/routes/image_processing/retrieveImage.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const multer = require('multer');
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+const dotenv = require('dotenv');
+
+const Event = require('../../models/events');
+const Organization = require('../../models/organization');
+const UserStudent = require('../../models/userStudent');
+
+// Encryption key and algorithm
+const algorithm = 'aes-256-ctr';
+const secretKey = process.env.secretKey_images;
+const iv = process.env.iv_images;
+
+router.get('/', async (req, res) => {
+        try {
+                const id = req.params.id;
+                const entityType = req.params.entityType;
+
+                const user = null;
+                switch (entityType) {
+                        case 'event':
+                                user = await Event.findById(id);
+                                break;
+                        case 'organization':
+                                user = await Organization.findById(id);
+                                break;
+                        case 'student':
+                                user = await UserStudent.findById(id);
+                                break;
+                        default:
+                                throw new Error('Invalid entity type');
+                }
+                
+                if (!user || !user.profilePicPath) {
+                        return res.status(404).send('Image not found or entity does not exist');
+                }
+
+                const filePath = user.profilePicPath; 
+
+                // Decrypt the file
+                const decryptedImage = decryptFile(filePath);
+
+                // update the response header with the content type
+                if (path.extname(filePath) === '.png') {
+                        res.setHeader('Content-Type', 'image/png');
+                } else if (path.extname(filePath) === '.jpg') {
+                        res.setHeader('Content-Type', 'image/jpeg');
+                } else {
+                        throw new Error('Invalid file type');
+                }
+
+                // Send the response with the decrypted image
+                res.send(decryptedImage);
+
+        } catch (error) {
+                console.error(error);
+                res.status(500).send('An error occurred');
+        }
+});
+
+function decryptFile(filePath) {
+        const encryptedImage = fs.readFileSync(filePath);
+        const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey, 'hex'), iv);
+        const decrypted = Buffer.concat([decipher.update(encryptedImage), decipher.final()]);
+        return decrypted;
+}
+
+module.exports = router;
