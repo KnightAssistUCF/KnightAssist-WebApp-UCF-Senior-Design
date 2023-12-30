93ad0aab93970cd7bc9049bd106879d4ceb07dbc
NSel1727 <noahseligson18@gmail.com>
2023-12-30 13:04:12 -0500

    Fix styling and add button for studentHistory

--- frontend/src/components/StudentHistory/StudentHistory.js
+++ frontend/src/components/StudentHistory/StudentHistory.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useContext } from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Logo from '../Logo';
 import Chip from '@mui/material/Chip';
 import { buildPath } from '../../path';
@@ -8,11 +10,13 @@ import Pagination from '@mui/material/Pagination';
 import Header from '../StudentHome/StudentHeader';
 import Avatar from '@mui/material/Avatar';
 import { List, ListItem, ListItemText, Divider } from '@mui/material';
+import {Button} from '@mui/material';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './StudentHistory.css'
 
 function StudentHistory()
 {
+
 	const [eventHistories, setEventHistories] = useState([]);
 	const [shownHistories, setShownHistories] = useState();
     const [numPages, setNumPages] = useState(0);  
@@ -21,26 +25,53 @@ function StudentHistory()
 	// Max number of event histories per page
 	const [amtPerPage, setAmtPerPage] = useState(5);
 
-    function changePage(e, value){
+    async function changePage(e, value){
         setPage(value);
 
 		const histories = eventHistories.slice(amtPerPage * (value - 1), amtPerPage * (value - 1) + amtPerPage);
-		setShownHistories(histories.map((history, i) => 	
-			<div>
-				{(i == 0) ? <Divider sx={{width: "100%", background: "black"}}/> : ""}
-				<ListItem>
-					<ListItemText primary={<span style={{ whiteSpace: 'pre-wrap' }}>
-											{"Event Attended: " + history.name}
-										  </span>} 						
- 								secondary={<span style={{ whiteSpace: 'pre-wrap' }}>
-									{"Check In: " + history.checkIn[0] + " at " + history.checkIn[1] 
-								 	+ "\nCheck Out: " + history.checkOut[0] + " at " + history.checkOut[1] + "\nHours Accumulated: +" + history.hours}
-								 		</span>}
-					/>
-				</ListItem>				
-				<Divider sx={{background: "black"}}/>
-			</div>
-		))
+		
+		for(let i = 0; i < histories.length; i++){
+			const history = histories[i];
+
+			const url = buildPath(`api/retrieveImage?entityType=event&id=${history.ID}`);
+
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {"Content-Type": "application/json"},
+			});
+	
+			let pic =  await response.blob();
+
+			histories[i] =  <div>
+							{(i == 0) ? <Divider sx={{width: "100%", background: "black"}}/> : ""}
+							<ListItem>
+								<Grid container layout={'row'} className='listsItems'>
+									<Grid item>
+										<Avatar
+											src={URL.createObjectURL(pic)}
+											className="eventPic"
+										/>
+									</Grid>
+									<Grid item sm={4}>
+										<ListItemText className='historyDetails' primary={<span style={{ whiteSpace: 'pre-wrap' }}>
+																{history.name + " (" + history.org + ")"}
+															</span>} 						
+													secondary={<span style={{ whiteSpace: 'pre-wrap' }}>
+														{"Check In: " + history.checkIn[0] + " at " + history.checkIn[1] 
+														+ "\nCheck Out: " + history.checkOut[0] + " at " + history.checkOut[1] + "\nHours Accumulated: +" + history.hours}
+															</span>}
+										/>
+									</Grid>
+									<Grid item>
+										<Button sx={{ mt: 5, ml:10, width: 150, backgroundColor: "#5f5395", "&:hover": {backgroundColor: "#7566b4"}}} variant="contained" onClick={null}>Event Info</Button>
+									</Grid>
+								</Grid>
+							</ListItem>
+							<Divider sx={{background: "black"}}/>
+						</div>
+		}
+
+		setShownHistories(histories);
 	}
 
 	async function getHistory(){        
@@ -52,7 +83,7 @@ function StudentHistory()
         });
     
         let res = JSON.parse(await response.text());
-
+		
 		// Sort by time if date is equal, date otherwise
 		res.sort(function(a, b) {
 			return Date.parse(a.checkIn[0] + " " + a.checkIn[1]) - Date.parse(b.checkIn[0] + " " + b.checkIn[1]);
@@ -80,18 +111,27 @@ function StudentHistory()
 			histories[i] =  <div>
 							{(i == 0) ? <Divider sx={{width: "100%", background: "black"}}/> : ""}
 							<ListItem>
-								<Avatar
-									src={URL.createObjectURL(pic)}
-									className="profilePic"
-								/>
-								<ListItemText className='historyDetails' primary={<span style={{ whiteSpace: 'pre-wrap' }}>
-														{history.name + " (" + history.org + ")"}
-													</span>} 						
-											secondary={<span style={{ whiteSpace: 'pre-wrap' }}>
-												{"Check In: " + history.checkIn[0] + " at " + history.checkIn[1] 
-												+ "\nCheck Out: " + history.checkOut[0] + " at " + history.checkOut[1] + "\nHours Accumulated: +" + history.hours}
-													</span>}
-								/>
+								<Grid container layout={'row'} className='listsItems'>
+									<Grid item>
+										<Avatar
+											src={URL.createObjectURL(pic)}
+											className="eventPic"
+										/>
+									</Grid>
+									<Grid item sm={4}>
+										<ListItemText className='historyDetails' primary={<span style={{ whiteSpace: 'pre-wrap' }}>
+																{history.name + " (" + history.org + ")"}
+															</span>} 						
+													secondary={<span style={{ whiteSpace: 'pre-wrap' }}>
+														{"Check In: " + history.checkIn[0] + " at " + history.checkIn[1] 
+														+ "\nCheck Out: " + history.checkOut[0] + " at " + history.checkOut[1] + "\nHours Accumulated: +" + history.hours}
+															</span>}
+										/>
+									</Grid>
+									<Grid item>
+										<Button sx={{ mt: 5, ml:10, width: 150, backgroundColor: "#5f5395", "&:hover": {backgroundColor: "#7566b4"}}} variant="contained" onClick={null}>Event Info</Button>
+									</Grid>
+								</Grid>
 							</ListItem>
 							<Divider sx={{background: "black"}}/>
 						</div>
