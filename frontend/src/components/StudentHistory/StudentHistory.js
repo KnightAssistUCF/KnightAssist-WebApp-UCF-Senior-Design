c84dee2ac122ad7f7a87159c83044b48f6a6ee66
AnishaRan <anisharanjan55@gmail.com>
2023-12-30 17:44:40 -0500

    prevent error for 404 not found response

--- frontend/src/components/StudentHistory/StudentHistory.js
+++ frontend/src/components/StudentHistory/StudentHistory.js
@@ -83,71 +83,77 @@ function StudentHistory()
 		setShownHistories(histories);
 	}
 
-	async function getHistory(){        
-        let url = buildPath(`api/historyOfEvents_User?studentId=${localStorage.getItem("ID")}`);
+	async function getHistory(){ 
+		try {
+		   
+			let url = buildPath(`api/historyOfEvents_User?studentId=${localStorage.getItem("ID")}`);
 
-        let response = await fetch(url, {
-            method: "GET",
-            headers: {"Content-Type": "application/json"},
-        });
-    
-        let res = JSON.parse(await response.text());
-
-		// Sort by time if date is equal, date otherwise
-		res.sort(function(a, b) {
-			return Date.parse(a.checkIn[0] + " " + a.checkIn[1]) - Date.parse(b.checkIn[0] + " " + b.checkIn[1]);
-		});
-
-        setEventHistories(res);
-		setNumPages(Math.ceil(res.length / amtPerPage));
-
-		const histories = res.slice(0, amtPerPage);
-
-		console.log(histories);
-
-		for(let i = 0; i < histories.length; i++){
-			const history = histories[i];
-
-			url = buildPath(`api/retrieveImage?entityType=event&id=${history.ID}`);
-
-			response = await fetch(url, {
+			let response = await fetch(url, {
 				method: "GET",
 				headers: {"Content-Type": "application/json"},
 			});
-	
-			let pic =  await response.blob();
+		
+			let res = JSON.parse(await response.text());
 
-			histories[i] =  <div>
-							{(i == 0) ? <Divider sx={{width: "100%", background: "black"}}/> : ""}
-							<ListItem>
-								<Grid container layout={'row'} className='listsItems'>
-									<Grid item sm={1}>
-										<Avatar
-											src={URL.createObjectURL(pic)}
-											className="eventPic"
-										/>
+			// Sort by time if date is equal, date otherwise
+			res.sort(function(a, b) {
+				return Date.parse(a.checkIn[0] + " " + a.checkIn[1]) - Date.parse(b.checkIn[0] + " " + b.checkIn[1]);
+			});
+
+			setEventHistories(res);
+			setNumPages(Math.ceil(res.length / amtPerPage));
+
+			const histories = res.slice(0, amtPerPage);
+
+			console.log(histories);
+
+			for(let i = 0; i < histories.length; i++){
+				const history = histories[i];
+
+				url = buildPath(`api/retrieveImage?entityType=event&id=${history.ID}`);
+
+				response = await fetch(url, {
+					method: "GET",
+					headers: {"Content-Type": "application/json"},
+				});
+		
+				let pic =  await response.blob();
+
+				histories[i] =  <div>
+								{(i == 0) ? <Divider sx={{width: "100%", background: "black"}}/> : ""}
+								<ListItem>
+									<Grid container layout={'row'} className='listsItems'>
+										<Grid item sm={1}>
+											<Avatar
+												src={URL.createObjectURL(pic)}
+												className="eventPic"
+											/>
+										</Grid>
+										<Grid item sm={4}>
+											<ListItemText className='historyDetails' primary={<span style={{ whiteSpace: 'pre-wrap' }}>
+																	{history.name + " (" + history.org + ")"}
+																</span>} 						
+														secondary={<span style={{ whiteSpace: 'pre-wrap' }}>
+															{"Check In: " + history.checkIn[0] + " at " + history.checkIn[1] 
+															+ "\nCheck Out: " + history.checkOut[0] + " at " + history.checkOut[1] + "\nHours Accumulated: +" + history.hours}
+																</span>}
+											/>
+										</Grid>
+										<Grid item>
+											<Button sx={{ mt: 4, ml:6, width: 150, backgroundColor: "#5f5395", "&:hover": {backgroundColor: "#7566b4"}}} variant="contained" onClick={() => setUpModal(history.ID)}>Event Info</Button>
+										</Grid>
 									</Grid>
-									<Grid item sm={4}>
-										<ListItemText className='historyDetails' primary={<span style={{ whiteSpace: 'pre-wrap' }}>
-																{history.name + " (" + history.org + ")"}
-															</span>} 						
-													secondary={<span style={{ whiteSpace: 'pre-wrap' }}>
-														{"Check In: " + history.checkIn[0] + " at " + history.checkIn[1] 
-														+ "\nCheck Out: " + history.checkOut[0] + " at " + history.checkOut[1] + "\nHours Accumulated: +" + history.hours}
-															</span>}
-										/>
-									</Grid>
-									<Grid item>
-										<Button sx={{ mt: 4, ml:6, width: 150, backgroundColor: "#5f5395", "&:hover": {backgroundColor: "#7566b4"}}} variant="contained" onClick={() => setUpModal(history.ID)}>Event Info</Button>
-									</Grid>
-								</Grid>
-							</ListItem>
-							<Divider sx={{background: "black"}}/>
-						</div>
-		}
+								</ListItem>
+								<Divider sx={{background: "black"}}/>
+							</div>
+			}
 
 
-		setShownHistories(histories);
+			setShownHistories(histories);
+
+		} catch (e) {
+			console.log("failed");
+		}  
 	}
 
     function Title(){
