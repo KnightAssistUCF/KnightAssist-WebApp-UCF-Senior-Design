7e66ed034f9fd7b54baf2ea83eac5f5600f33ed6
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2023-12-30 18:19:20 -0500

    fixed login to include admins as well

--- backend/routes/volunteers/Login.js
+++ backend/routes/volunteers/Login.js
@@ -6,12 +6,29 @@ const { authenticateToken_User, authenticateToken_Organization } = require('../.
 
 const userStudent = require('../../models/userStudent');
 const organization = require('../../models/organization');
+const admin = require('../../models/admin');
 
 router.post('/', async (req, res) => {
     const loginEmail = req.body.email;
     const loginPassword = req.body.password;
 
     try {
+
+        /* [ADMIN] Only */
+        let admin = await admin.findOne({ email: loginEmail });
+
+        if (admin) {
+            const isPasswordValid = await bcrypt.compare(loginPassword, admin.password);
+            if (isPasswordValid) {
+                const token = generateToken({ email: loginEmail}, process.env.JWT_SECRET_KEY);
+                return res.status(200).set("authorization", token).send({admin, "token": token}); 
+            } else {
+                return res.status(400).send("Invalid password");
+            }
+        }
+        /* [ADMIN] Only */
+
+        
         let user = await userStudent.findOne({ email: loginEmail });
 
         if (user) {
