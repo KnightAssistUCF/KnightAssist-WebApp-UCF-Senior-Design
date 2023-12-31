42e79a40f9ca923bd935988c72a07a6c57491485
AnishaRan <anisharanjan55@gmail.com>
2023-12-31 18:02:35 -0500

    saving changes

--- frontend/src/components/Admin/StudentTable.js
+++ frontend/src/components/Admin/StudentTable.js
@@ -1,4 +1,11 @@
 import {useState} from 'react';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
 
 
 function StudentTable()
