12daaf3065566ff0f79b1ddc8f6e27ea319b2655
AnishaRan <anisharanjan55@gmail.com>
2024-01-02 22:31:26 -0500

    adding sorting for student table

--- frontend/src/components/Admin/StudentTable.js
+++ frontend/src/components/Admin/StudentTable.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -9,45 +9,114 @@ import TableSortLabel from '@mui/material/TableSortLabel';
 import Table from '@mui/material/Table';
 import './AdminHome.css';
 
+function StudentTable(props) {
+  const [orderBy, setOrderBy] = useState('createdAt');
+  const [order, setOrder] = useState('desc');
 
+  const handleRequestSort = (property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
 
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  };
 
-function StudentTable(props)
-{
-  
+  const getComparator = (order, orderBy) => {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  };
 
-  return(
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+
+  return (
     <div>
       <TableContainer className="tableContainer">
         <Table className="studentTable">
           <TableHead>
             <TableRow>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Total Volunteer Hours</TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={orderBy === 'firstName'}
+                  direction={orderBy === 'firstName' ? order : 'asc'}
+                  onClick={() => handleRequestSort('firstName')}
+                >
+                  First Name
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={orderBy === 'lastName'}
+                  direction={orderBy === 'lastName' ? order : 'asc'}
+                  onClick={() => handleRequestSort('lastName')}
+                >
+                  Last Name
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={orderBy === 'createdAt'}
+                  direction={orderBy === 'createdAt' ? order : 'asc'}
+                  onClick={() => handleRequestSort('createdAt')}
+                >
+                  Created
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={orderBy === 'email'}
+                  direction={orderBy === 'email' ? order : 'asc'}
+                  onClick={() => handleRequestSort('email')}
+                >
+                  Email
+                </TableSortLabel>
+              </TableCell>
+              <TableCell>
+                <TableSortLabel
+                  active={orderBy === 'totalVolunteerHours'}
+                  direction={
+                    orderBy === 'totalVolunteerHours' ? order : 'asc'
+                  }
+                  onClick={() => handleRequestSort('totalVolunteerHours')}
+                >
+                  Total Volunteer Hours
+                </TableSortLabel>
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-          {props.students.length > 0 ? (
-            props.students.map((student) => (
-              <TableRow key={student._id}>
-                <TableCell>{student.firstName}</TableCell>
-                <TableCell>{student.lastName}</TableCell>
-                <TableCell>{student.email}</TableCell>
-                <TableCell>{student.totalVolunteerHours}</TableCell>
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={3}>No students available</TableCell>
-            </TableRow>
-          )}
+            {stableSort(props.students, getComparator(order, orderBy)).map(
+              (student) => (
+                <TableRow key={student._id}>
+                  <TableCell>{student.firstName}</TableCell>
+                  <TableCell>{student.lastName}</TableCell>
+                  <TableCell>{student.createdAt}</TableCell>
+                  <TableCell>{student.email}</TableCell>
+                  <TableCell>{student.totalVolunteerHours}</TableCell>
+                </TableRow>
+              )
+            )}
           </TableBody>
         </Table>
       </TableContainer>
     </div>
   );
-};
+}
 
 export default StudentTable;
