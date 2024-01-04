09bd9d7a135cdc636e29d681177a5ca037a485d1
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-01-03 21:53:48 -0500

    fixed small bug with markAsRead endpoint

--- backend/routes/FeedbackAndReviews/markAsRead.js
+++ backend/routes/FeedbackAndReviews/markAsRead.js
@@ -27,9 +27,4 @@ router.put('/', async (req, res) => {
     }
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+module.exports = router;
