3109e57a239660eb579fefbb2903cc3f15c1117c
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-01-03 21:24:30 -0500

    create the add feedback endpoint (needs testing)

---
+++ backend/routes/FeedbackAndReviews/addFeedback.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+const Event = mongoose.model('Event', eventSchema);
+
+router.post('/', async (req, res) => {
+    try {
+        const eventID = req.body.eventID;
+        const studentID = req.body.studentID; 
+        const studentName = req.body.studentName;
+        const rating = req.body.rating;
+        const feedbackText = req.body.feedbackText;
+
+        const event = await Event.findById(eventID);
+        if (!event) {
+            return res.status(404).send('Event not found in the database or the ID provided is incorrect!');
+        }
+
+        let feedbackData = {
+            studentId: studentID,
+            studentName: studentName,
+            rating: rating,
+            feedbackText: feedbackText
+        };
+        // Create a new feedback instance and fill it with data and save it 
+        const feedback = new Feedback(feedbackData);
+        event.feedback.push(feedback);
+        await event.save();
+
+        res.status(201).send("Feedback from volunteer: " + studentName + " has been added to event " + event.name + " successfully!");
+    } catch (error) {
+        res.status(500).send("An error occurred: " + error.message);
+    }
+});
+
+module.exports = router;
