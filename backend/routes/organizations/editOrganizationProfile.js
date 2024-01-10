db05f6b20e269a3875cabb8c2660007c3a2a78c4
NSel1727 <noahseligson18@gmail.com>
2024-01-10 10:50:31 -0500

    Ensure null fields don't override values in edit APIs

--- backend/routes/organizations/editOrganizationProfile.js
+++ backend/routes/organizations/editOrganizationProfile.js
@@ -21,24 +21,23 @@ router.post('/', authenticateToken_Organization, async (req, res) => {
     await organization.findOne(query).then((user) => {
         console.log(user); // debugging
         if (user) {
-            var newHashedPassword = bcryptjs.hashSync(req.body.password, 10);
-            user.name = req.body.name;
-            user._id = req.body._id;
-            user.email = req.body.email;
+            var newHashedPassword = (req.body.password) ? bcryptjs.hashSync(req.body.password, 10) : user.password;
+            user.name = (req.body.name) ? req.body.name : user.name;
+            user.email = (req.body.email) ? req.body.email : user.email;
             user.password = newHashedPassword;
-            user.description = req.body.description;
-            user.logoUrl = req.body.logoUrl;
-            user.followers = req.body.followers; // I doubt these work as of yet
-            user.favorites = req.body.favorites; // I doubt these work as of yet
-            user.updates = req.body.updates;
-            user.calendarLink = req.body.calendarLink;
-            user.contact = req.body.contact;
-            user.isActive = req.body.isActive;
-            user.eventHappeningNow = req.body.eventHappeningNow;
-            user.backgroundURL = req.body.backgroundURL;
-            user.eventsArray = req.body.eventsArray;
-            user.location = req.body.location;
-            user.categoryTags = req.body.categoryTags;
+            user.description = (req.body.description) ? req.body.description : user.description;
+            user.logoUrl = (req.body.logoUrl) ? req.body.logoUrl : user.logoUrl;
+            user.followers = (req.body.followers) ? req.body.followers : user.followers; 
+            user.favorites = (req.body.favorites) ? req.body.favorites : user.favorites; 
+            user.updates = (req.body.updates) ? req.body.updates : user.updates;
+            user.calendarLink = (req.body.calendarLink) ? req.body.calendarLink : user.calendarLink;
+            user.contact = (req.body.contact) ? req.body.contact : user.contact;
+            user.isActive = (req.body.isActive) ? req.body.isActive : user.isActive;
+            user.eventHappeningNow = (req.body.eventHappeningNow) ? req.body.eventHappeningNow : user.eventHappeningNow;
+            user.backgroundURL = (req.body.backgroundURL) ? req.body.backgroundURL : user.backgroundURL;
+            user.eventsArray = (req.body.eventsArray) ? req.body.eventsArray : user.eventsArray;
+            user.location = (req.body.location) ? req.body.location : user.location;
+            user.categoryTags = (req.body.categoryTags) ? req.body.categoryTags : user.categoryTags;
             /* we will add more here as is needed later once we determine what actually can 
             be editatble */
             user.save();
