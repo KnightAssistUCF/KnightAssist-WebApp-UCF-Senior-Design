e3cbbe227ddefd05c168bf132ba240d7ddd3ad0f
NSel1727 <noahseligson18@gmail.com>
2024-01-09 21:30:11 -0500

    Start additions to postverify page

--- frontend/src/components/PostVerifiedQuestions/PostVerifiedQuestions.js
+++ frontend/src/components/PostVerifiedQuestions/PostVerifiedQuestions.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Logo from '../Logo';
 import Chip from '@mui/material/Chip';
+import Avatar from '@mui/material/Avatar';
 import { buildPath } from '../../path';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './PostVerifiedQuestions.css'
@@ -15,9 +16,19 @@ function PostVerifiedQuestions()
     const [tags, setTags] = useState([]);
     const [colors, setColors] = useState(makeColorsArray());
     const [selectedTags, setSelectedTags] = useState([]);
+	const [picName, setPicName] = useState(null);
+	const [picFile, setPicFile] = useState(null);
+	const [bgName, setBGName] = useState(null);
+	const [bgFile, setBGFile] = useState(null);
+
+
+	const [role, setRole] = useState(undefined);
 
     // State needed due to bug where tag names where undefined
     const [makeTags, setMakeTags] = useState([]);
+
+	const backgroundSelect = useRef(null);
+	const profilePicSelect = useRef(null);
 
 	function handleClick(idx){
 		if(colors[idx] !== "default"){
@@ -41,24 +52,18 @@ function PostVerifiedQuestions()
 		for(let i = 0; i < 50; i++)
 			colors.push("default");
 
-		setColors(colors);
-
 		return colors;
-    }
-
-    function getAccountType(){
-	
     }
 
     async function getTagNames(){
 		let url = buildPath(`api/getAllAvailableTags`);
 
-			let response = await fetch(url, {
-				method: "GET",
-				headers: {"Content-Type": "application/json"},
-			});
-		
-			let res = JSON.parse(await response.text());
+		let response = await fetch(url, {
+			method: "GET",
+			headers: {"Content-Type": "application/json"},
+		});
+	
+		let res = JSON.parse(await response.text());
 
 		setTagNames(res);
 
@@ -98,45 +103,91 @@ function PostVerifiedQuestions()
     }
 
     function Header(){
-	return(
-	    <div className='header'>
-		Additional Questions <Logo theStyle="logoStyle"/>
-	    </div>
-	);
+		return(
+			<div className='header'>
+				Additional Questions <Logo theStyle="logoStyle"/>
+			</div>
+		);
     }
 
     function SemesterGoal(){
-	return (
-	    <div className='semesterGoal'>
-		<Grid container justify="center" alignItems="center" marginBottom={"50px"}>
-		    <p className='goalQuestion'>What is your semester-wide hourly volunteer goal?</p>
-		    <TextField
-			name={"Hourly Goal"}
-			required
-			label={"Hourly Goal"}
-			autoFocus
-			multiline={false}
-			onChange={(e) => {e.currentTarget.value = e.target.value.replace(/[\D\s]/, ''); setHourlyGoal(e.target.value)}}
-			value={hourlyGoal}
-		    />
-		</Grid>
-	    </div>
-	)
+		return (
+			<div className='semesterGoal'>
+				<Grid container justify="center" alignItems="center" marginBottom={"50px"}>
+					<p className='goalQuestion'>What is your semester-wide hourly volunteer goal?</p>
+					<TextField
+					name={"Hourly Goal"}
+					required
+					label={"Hourly Goal"}
+					multiline={false}
+					onChange={(e) => {e.currentTarget.value = e.target.value.replace(/[\D\s]/, ''); setHourlyGoal(e.target.value)}}
+					value={hourlyGoal}
+					/>
+				</Grid>
+			</div>
+		)
     }
+
+	function validateImgSelection(fileSelect){
+		// No files selected
+		if(fileSelect.current.files.length === 0) return false;
+		
+		const file = fileSelect.current.files[0].name;
+		console.log(file)
+
+		const fileType = file.substring(file.lastIndexOf(".") + 1);
+
+		return fileType === "png" || fileType === "gif" || fileType === "jpg" || fileType === "jpeg";
+	}
+
+	function ProfilePicture(){
+		return (
+			<div className='profilePicSelect'>
+				<Grid container justify="center" alignItems="center" marginBottom={"50px"}>
+					<Avatar
+						src={(picName !== null) ? URL.createObjectURL(picName) : "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"}
+						className="picStyle"
+					/>
+					<label for="profilePic" className="selectPPic btn btn-primary">Select Profie Picture</label>
+					<input ref={profilePicSelect} id="profilePic" type="file" accept="image/png, image/gif, image/jpg image/jpeg" style={{display:"none"}} onChange={() => {if(validateImgSelection(profilePicSelect)){setPicName(profilePicSelect.current.files[0]); setPicFile(profilePicSelect.current.files[0])}}}/>
+				</Grid>
+			</div>
+		)
+	}
+
+	function BackgroundPic(){
+		return (
+			<div className='profilePicSelect'>
+				<Grid container justify="center" alignItems="center" marginBottom={"50px"}>
+					<Avatar
+						src={(bgName !== null) ? URL.createObjectURL(bgName) : "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"}
+						className="picStyle"
+					/>
+					<label for="background" className="selectPPic btn btn-primary">Select Background</label>
+					<input ref={backgroundSelect} id="background" type="file" accept="image/png, image/gif, image/jpg image/jpeg" style={{display:"none"}} onChange={() => {if(validateImgSelection(backgroundSelect)){setBGName(backgroundSelect.current.files[0]); setBGFile(backgroundSelect.current.files[0])}}}/>
+				</Grid>
+			</div>
+		)
+	}
 
     function AllTags(){
 		return (
 			<div>
-			<p className='tagQuestion'>Select up to 10 of the below interests:</p>
-			<div className='allTags'>
-				{tags}
-			</div>
+				<p className='tagQuestion'>Select up to 10 of the below interests:</p>
+				<div className='allTags'>
+					{tags}
+				</div>
 			</div>
 		)
     }
 
     useEffect(()=>{
-        getAccountType();
+        if(localStorage.getItem("role") === "volunteer"){
+			setRole("volunteer");
+		}else{
+			setRole("organization");
+		}
+
 		getTagNames();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
@@ -150,10 +201,12 @@ function PostVerifiedQuestions()
 
     return(
       <div id='homePage'>
-	<Header/>
-	<SemesterGoal/>
-	<AllTags/>
-	<button type="button" class="submitBtn btn btn-primary" onClick={() => submit()}>Submit</button>
+		<Header/>
+		{ProfilePicture()}
+		{(role === "organization") ? BackgroundPic() : ""}
+		<SemesterGoal/>
+		<AllTags/>
+		<button type="button" class="submitBtn btn btn-primary" onClick={() => submit()}>Submit</button>
       </div>
     );
 };
