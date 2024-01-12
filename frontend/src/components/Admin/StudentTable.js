070c449f166225db226011f956f8f820aa49f29e
AnishaRan <anisharanjan55@gmail.com>
2024-01-12 17:37:16 -0500

    changing pagination default values

--- frontend/src/components/Admin/StudentTable.js
+++ frontend/src/components/Admin/StudentTable.js
@@ -58,7 +58,7 @@ function StudentTable(props) {
 
 
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rowsPerPage, setRowsPerPage] = React.useState(6);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -146,7 +146,7 @@ function StudentTable(props) {
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[10, 15, 25]}
+        rowsPerPageOptions={[6, 10, 20]}
         component="div"
         count={props.students.length}
         rowsPerPage={rowsPerPage}
