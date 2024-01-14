12cf3d7c9fc5de7a284ff369f800889bafb95226
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-01-14 16:27:06 -0500

    fix bugs with event default picture

--- backend/routes/image_processing/retrieveImage.js
+++ backend/routes/image_processing/retrieveImage.js
@@ -22,20 +22,21 @@ router.get('/', async (req, res) => {
 
                 const profilePicOrBackGround = req.query.profilePicOrBackGround; // always 0 for profile pic and 1 for background (background only for org)
 
-				console.log('entityType: ', entityType);
+                console.log('entityType: ', entityType);
                 console.log('id: ', id);
 
                 let user;
-				let defaultPath_Background;
+                let defaultPath_Background;
                 let defaultPath_ProfilePic;
 
                 switch (entityType) {
                         case 'event':
                                 user = await Event.findById(id);
+                                defaultPath_ProfilePic = 'backend/images/orgdefaultbackground.png'
                                 break;
                         case 'organization':
                                 user = await Organization.findById(id);
-				defaultPath_Background = 'backend/images/orgdefaultbackground.png'
+                                defaultPath_Background = 'backend/images/orgdefaultbackground.png'
                                 defaultPath_ProfilePic = 'backend/images/defaultProfilePic.png'
                                 break;
                         case 'student':
@@ -44,10 +45,10 @@ router.get('/', async (req, res) => {
                         default:
                                 throw new Error('Invalid entity type');
                 }
-                
-                if (!user 
-                || (entityType !== 'organization' && !user.profilePicPath)
-                || (entityType === 'organization' && !user && (!user.profilePicPath || !user.backgroundPicPath))) {
+
+                if (!user
+                        || (entityType !== 'organization' && !user.profilePicPath)
+                        || (entityType === 'organization' && !user && (!user.profilePicPath || !user.backgroundPicPath))) {
                         return res.status(404).send('Image not found or entity does not exist');
                 }
 
@@ -67,31 +68,29 @@ router.get('/', async (req, res) => {
                         filePath = path.normalize(user.profilePicPath);
                 }
 
-                
-                console.log(filePath);
 
                 // converts the path to be a neutral format 
-               /* const normalizedPath = user.profilePicPath.replace(/\\/g, '/');
-                const baseDir = path.join(__dirname, '..', '..', 'backend');
-                const filePath = path.join(baseDir, normalizedPath);*/
+                /* const normalizedPath = user.profilePicPath.replace(/\\/g, '/');
+                 const baseDir = path.join(__dirname, '..', '..', 'backend');
+                 const filePath = path.join(baseDir, normalizedPath);*/
 
-                if(entityType === 'organization' && profilePicOrBackGround === '1' && user.backgroundURL === defaultPath_Background){
+                if (entityType === 'organization' && profilePicOrBackGround === '1' && user.backgroundURL === defaultPath_Background) {
                         res.send(fs.readFileSync(filePath));
-                } else if (entityType === 'organization' && profilePicOrBackGround === '0' && user.profilePicPath === defaultPath_ProfilePic){
+                } else if (entityType === 'organization' && profilePicOrBackGround === '0' && user.profilePicPath === defaultPath_ProfilePic) {
                         res.send(fs.readFileSync(filePath));
-                } else if (entityType !== 'organization' && user.profilePicPath === defaultPath_ProfilePic){
+                } else if (entityType !== 'organization' && user.profilePicPath === defaultPath_ProfilePic) {
                         res.send(fs.readFileSync(filePath));
-                } else{
+                } else {
                         // Decrypt the file
                         const decryptedImage = decryptFile(filePath);
 
                         // update the response header with the content type
                         if (path.extname(filePath) === '.png') {
-                                        res.setHeader('Content-Type', 'image/png');
+                                res.setHeader('Content-Type', 'image/png');
                         } else if (path.extname(filePath) === '.jpg') {
-                                        res.setHeader('Content-Type', 'image/jpeg');
+                                res.setHeader('Content-Type', 'image/jpeg');
                         } else {
-                                        throw new Error('Invalid file type');
+                                throw new Error('Invalid file type');
                         }
 
                         // Send the response with the decrypted image
