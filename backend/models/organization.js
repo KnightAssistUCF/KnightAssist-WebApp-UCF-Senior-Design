9bdf99d68a946c1f55db20534d097d64ee3dad14
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-01-15 18:13:23 -0500

    removed followers across the database and kept favorited students

--- backend/models/organization.js
+++ backend/models/organization.js
@@ -31,10 +31,11 @@ const organizationSchema = new Schema({
     },
     logoUrl: String,
     categoryTags: [String], // store tags for org
-    followers: [{
-        type: Schema.Types.ObjectId, // people that follow this organization
-        ref: 'userStudent',
-    }],
+    // [Removed this for now as we do not use it]
+    // followers: [{
+    //     type: Schema.Types.ObjectId, // people that follow this organization
+    //     ref: 'userStudent',
+    // }],
     favorites: [{
         type: Schema.Types.ObjectId, // people that liked and favorited this organization
         ref: 'userStudent',
