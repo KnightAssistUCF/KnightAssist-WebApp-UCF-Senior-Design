1fce8f7782382f90b6b7b2d08843c5924d448b90
NSel1727 <noahseligson18@gmail.com>
2024-01-16 00:06:09 -0500

    Add banner image, css fix

--- frontend/src/components/OrgProfile/OrgProfile.js
+++ frontend/src/components/OrgProfile/OrgProfile.js
@@ -4,13 +4,14 @@ import StudentHeader from '../StudentHome/StudentHeader'
 import './OrgProfile.css';
 import OrgTopBar from '../OrgHome/OrgTopBar';
 import Card from '@mui/material/Card';
-import { Button, Typography, CardContent } from '@mui/material';
+import { Button, Typography, CardContent, CardMedia } from '@mui/material';
 import { buildPath } from '../../path';
 import NavTabs from './NavTabs';
 import OrgBox from './OrgBox';
 
 function OrgProfile() {
 	const [org, setOrg] = useState(null);
+	const [bgFile, setBGFile] = useState(null);
 
 	async function getOrgInfo(){
         let organizationID;
@@ -34,6 +35,27 @@ function OrgProfile() {
 		console.log(res);
 		
 		setOrg(res);
+
+		url = buildPath(`api/retrieveImage?entityType=organization&id=${organizationID}&profilePicOrBackGround=1`);
+
+		response = await fetch(url, {
+			method: "GET",
+			headers: {"Content-Type": "application/json"},
+		});
+
+		let background = await response.blob();
+
+		setBGFile(background);
+	}
+
+	function BackgroundBanner(){
+		return (
+				<CardMedia
+					className='orgBannerFiller'
+					component="img"
+					image={(bgFile !== null) ? URL.createObjectURL(bgFile) : ""}
+				/>				
+		)
 	}
 
 	useEffect(() => {
@@ -48,7 +70,7 @@ function OrgProfile() {
 			{(sessionStorage.getItem("role") === "organization") ? <Header /> : <StudentHeader/>}
 			<div className='orgProfilePage spartan'>
 				{/* <div className='orgProfileTitle'>Organization Profile</div> */}
-				<div className='orgBannerFiller'></div>
+				<BackgroundBanner/>
 				<OrgBox org={org}/>
 				<div className='navTabs'>
 					<NavTabs/>
