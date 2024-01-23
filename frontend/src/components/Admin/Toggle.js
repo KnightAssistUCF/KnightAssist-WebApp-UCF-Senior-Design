60fcd4fc1805b3bcd94ab4e54caed368bc590a83
AnishaRan <anisharanjan55@gmail.com>
2024-01-22 22:29:05 -0500

    changing toggle button colors

--- frontend/src/components/Admin/Toggle.js
+++ frontend/src/components/Admin/Toggle.js
@@ -1,23 +1,19 @@
 import * as React from 'react';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import OrganizationTable from './OrgTable';
-import StudentTable from './StudentTable';
 
 export default function ColorToggleButton({ onToggleChange }) {
     const [alignment, setAlignment] = React.useState('student');
 
     const handleChange = (event, newAlignment) => {
-      if (newAlignment !== null) {
-          setAlignment(newAlignment);
-          onToggleChange(newAlignment);
-      }
-  };
+        setAlignment(newAlignment);
+        onToggleChange(newAlignment);
+    };
 
   return (
     <div>
         <ToggleButtonGroup
-        color="primary"
+        color="success"
         value={alignment}
         exclusive
         size='small'
@@ -25,8 +21,18 @@ export default function ColorToggleButton({ onToggleChange }) {
         aria-label="Table"
         sx={{ paddingLeft: '30px' }}
         >
-        <ToggleButton value="student">Student</ToggleButton>
-        <ToggleButton value="organization">Organization</ToggleButton>
+        <ToggleButton value="student" sx={{
+                        color: 'gray',
+                        '&.Mui-selected': { color: 'white', backgroundColor: '#00664D' },
+                        '&:hover': { backgroundColor: '#FAFAFA' },
+                        '&.Mui-selected:hover': { backgroundColor: '#00664D' },
+                    }}>Student View</ToggleButton>
+        <ToggleButton value="organization" sx={{
+                        color: 'gray',
+                        '&.Mui-selected': { color: 'white', backgroundColor: '#00664D' },
+                        '&:hover': { backgroundColor: '#FAFAFA' },
+                        '&.Mui-selected:hover': { backgroundColor: '#00664D' },
+                    }}>Organization View</ToggleButton>
         </ToggleButtonGroup>
     </div>
   );
