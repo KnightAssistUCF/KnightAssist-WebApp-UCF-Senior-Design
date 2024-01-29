85d018a258927bec0da996d87ce66a7abfd7399d
NSel1727 <noahseligson18@gmail.com>
2024-01-29 12:10:50 -0500

    Add functional buttons to topbar

--- frontend/src/components/TopBar/StudentTopBar.js
+++ frontend/src/components/TopBar/StudentTopBar.js
@@ -15,6 +15,7 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import Badge from '@mui/material/Badge';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useNavigate } from 'react-router-dom';
 
 function StudentTopBar()
 {
@@ -22,20 +23,38 @@ function StudentTopBar()
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
 
+	const navigate = useNavigate();
+
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
-      };
-      const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-      };
-    
-      const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-      };
-    
-      const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-      };
+    };
+
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+
+
+	function handleButtonClick(setting){
+		if(setting === "Profile"){
+			sessionStorage.removeItem("viewingPageID");
+			if(window.location.href.substring(window.location.href.lastIndexOf("#")) === "#/studentprofile"){
+				window.location.reload();
+			}else{		
+				window.location.href = "#/studentprofile";
+			}
+		}else{
+			sessionStorage.clear();
+			navigate('/'); 
+		}
+	}
 
    return(
       <div className="StudentTopBar">
@@ -82,7 +101,7 @@ function StudentTopBar()
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <MenuItem key={setting} onClick={() => handleButtonClick(setting)}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
