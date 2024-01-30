112ea9fec291543996486bd9772bd1a4a8682f17
NSel1727 <noahseligson18@gmail.com>
2024-01-30 12:27:48 -0500

    Add tabs back to student profile, put recent events and fav orgs in it

--- frontend/src/components/StudentProfile/NavTabs.js
+++ frontend/src/components/StudentProfile/NavTabs.js
@@ -4,6 +4,50 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import StudentInfo from './StudentInfo';
+import RecentEvents from './RecentEvents';
+import FavoriteOrganizations from './FavoriteOrganizations';
+
+const AntTabs = styled(Tabs)({
+//   borderBottom: '1px solid #e8e8e8',
+	'& .MuiTabs-indicator': {
+	backgroundColor: '#1890ff',
+	},
+});
+
+const AntTab = styled((props) => <Tab disableRipple {...props} />)(({ theme }) => ({
+	textTransform: 'none',
+	minWidth: 0,
+	[theme.breakpoints.up('sm')]: {
+	minWidth: 0,
+	},
+	fontWeight: theme.typography.fontWeightRegular,
+	fontSize: '17px',
+	marginRight: theme.spacing(1),
+	color: 'rgba(0, 0, 0, 0.85)',
+	fontFamily: [
+	'-apple-system',
+	'BlinkMacSystemFont',
+	'"Segoe UI"',
+	'Roboto',
+	'"Helvetica Neue"',
+	'Arial',
+	'sans-serif',
+	'"Apple Color Emoji"',
+	'"Segoe UI Emoji"',
+	'"Segoe UI Symbol"',
+	].join(','),
+	'&:hover': {
+	color: '#40a9ff',
+	opacity: 1,
+	},
+	'&.Mui-selected': {
+	color: '#1890ff',
+	fontWeight: theme.typography.fontWeightMedium,
+	},
+	'&.Mui-focusVisible': {
+	backgroundColor: '#d1eaff',
+	},
+}));
 
 const BioTabContent = (props) => (
   <div>
@@ -23,7 +67,11 @@ export default function CustomizedTabs(props) {
     switch (tabValue) {
       case 0:
         return <BioTabContent user={props.user} editMode={props.editMode} editInfo={props.editInfo}/>;
-      default:
+      case 1:
+		return 	<RecentEvents setOpen={props.setOpenModal} setEventID={props.setEventID}/>					
+	  case 2:
+		return <FavoriteOrganizations/>
+	  default:
         return null;
     }
   };
@@ -31,6 +79,11 @@ export default function CustomizedTabs(props) {
   return (
     <Box >
       <Box sx={{ bgcolor: '#fff' }}>
+	 	<AntTabs value={value} onChange={handleChange} aria-label="ant example">
+			<AntTab label="Info"/>
+			<AntTab label="Recent Events"/>
+			<AntTab label="Favorite Orgs"/>
+       	</AntTabs>
         <Box sx={{ p: 3 }}>
           {getTabContent(value)}
         </Box>
