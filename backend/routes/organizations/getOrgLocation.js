e56351c5d96711e4ae9d3f9580a52bdcd720db17
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-01-29 21:21:46 -0500

    create an address fetch endpoint for orgs

---
+++ backend/routes/organizations/getOrgLocation.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const organization = require('../../models/organization');
+
+router.get('/', async (req, res) => {
+
+    try {
+        const query = {
+            $or: [
+                { _id: req.query.organizationID },
+                { email: req.query.email }
+            ]
+        };
+
+        const organizationUser = await organization.findOne(query);
+
+        if (!organizationUser) {
+            res.status(404).send("Organization not found in the database");
+        }
+
+        res.json(organizationUser.location);
+    } catch (err) {
+        res.status(503).send("Internal server error: " + err);
+    }
+
+});
+
+module.exports = router;
