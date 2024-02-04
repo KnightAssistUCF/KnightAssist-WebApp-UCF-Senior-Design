e4f64bda43bf918a1e6b7575ec5705c69290f77e
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-02-03 21:21:10 -0500

    create the leaderboard for all students across orgs (needs testing)

--- backend/routes/leaderBoard/allStudentsRanking.js
+++ backend/routes/leaderBoard/allStudentsRanking.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const UserStudent = require('../../models/userStudent');
+
+router.get('/', async (req, res) => {
+    try {
+        const rankedStudents = await UserStudent.find({})
+            .sort({ totalVolunteerHours: -1 }) // follow a descending order from highest volunteering guy to least
+            .select('firstName lastName totalVolunteerHours') // these are the fields we will show
+            .exec();
+
+        res.json({
+            success: true,
+            message: 'Students ranked by total volunteer hours',
+            data: rankedStudents
+        });
+    } catch (error) {
+        console.error('Error ranking students by total volunteering hours:', error);
+        res.status(500).send('Server error while fetching and ranking students.');
+    }
+});
+
+module.exports = router;
