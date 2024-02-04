e812b7cfaf41b2698b4a98646bcb941f4eb40045
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-02-04 08:38:24 -0500

    creating the perOrgLeaderBoard api

--- backend/routes/leaderBoard/perOrgLeaderBoard.js
+++ backend/routes/leaderBoard/perOrgLeaderBoard.js
@@ -5,6 +5,29 @@ const Organization = require('../../models/organization');
 
 router.get('/', async (req, res) => {
 
+    try {
+        
+        const orgId = req.query.orgId;
+
+        const organiztion = await Organiaztion.findById(orgId)
+        .populate({
+            path: 'favorites', model: 'userStudent',
+            select: 'firstName lastName totalVolunteerHours -_id', 
+            options: { sort: { 'totalVolunteerHours': -1 } } 
+        });
+
+        if (!organiztion) {
+            return res.status(404).send('Organization not found in the database.');
+        }
+
+        const result = organization.favorites;
+        res.json(result);
+
+    } catch {
+        console.error('Internal error: ', error);
+        res.status(500).send('Internal server error while making the per org leaderboard.');
+    }
+
 });
 
 module.exports = router;
