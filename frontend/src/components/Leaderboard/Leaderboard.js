4db42a69e4064d36d1da817a3e4b312cc96f98e9
NSel1727 <noahseligson18@gmail.com>
2024-02-08 14:36:08 -0500

    Add search design and implementation

--- frontend/src/components/Leaderboard/Leaderboard.js
+++ frontend/src/components/Leaderboard/Leaderboard.js
@@ -8,11 +8,14 @@ import StudentTopBar from '../TopBar/StudentTopBar';
 import { CoPresentOutlined } from '@mui/icons-material';
 import { Avatar, CardActionArea, CardContent, Grid, Typography } from '@mui/material';
 import { Card } from 'react-bootstrap';
+import { BsSearch } from 'react-icons/bs';
+import Search from './Search';
 
 function Leaderboard() {
 	const [role, setRole] = useState(sessionStorage.getItem("role"));
 	const [studentData, setStudentData] = useState(undefined);
 	const [yourData, setYourData] = useState(undefined);
+	const [searchID, setSearchID] = useState(undefined);
 
 	async function getStudentData(){
 		let url;
@@ -69,9 +72,67 @@ function Leaderboard() {
 	}
 
 	function YourRank(){
+
 		const student = yourData.data;
 		const place = yourData.rank;
 		const pic = yourData.pic;
+
+		return (
+			<Grid container justifyContent="center" alignItems="center">
+				<Card className={"rankCard purpleCard"} variant="outlined">
+					<CardContent>
+						<Avatar className='rankAvatar rankNumber rankItem' style={{border: '0.1px solid black'}}>{place}</Avatar>
+						<Avatar className='rankAvatar rankItem rankPic' style={{border: '0.1px solid black'}} src={(pic) ? URL.createObjectURL(pic) : "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"} />
+						<Typography
+							variant="body2"
+							color="textSecondary"
+							component="p"
+							className='rankItem rankName'
+							style={{ color: 'black'}}
+						>
+							<b>{student.firstName + " " + student.lastName}</b>
+						</Typography>
+						<Typography
+							variant="body2"
+							color="textSecondary"
+							component="p"
+							className='removeIfShort rankItem rankEvents'
+							style={{ color: 'black'}}
+						>
+							{student.eventsHistory.length} Events
+						</Typography>
+						<Typography
+							variant="body2"
+							color="textSecondary"
+							component="p"
+							className='removeIfShort rankItem'
+							style={{ color: 'black'}}
+						>
+							{student.totalVolunteerHours} Hours
+						</Typography>
+					</CardContent>
+				</Card>
+			</Grid>
+		)
+	}
+
+	function SearchRank(){
+		
+		let student, pic, place;
+		let i = 0;
+
+		for(let curStudent of studentData){
+
+			// It is the student that was searched for
+			if(curStudent[0]._id === searchID){
+				student = curStudent[0];
+				pic = curStudent[1];
+				place = i + 1;
+			}
+
+			i++;
+		}
+		
 
 		return (
 			<Grid container justifyContent="center" alignItems="center">
@@ -182,7 +243,9 @@ function Leaderboard() {
 			  <div className='rankDisplay'>
 				{(role === "volunteer" && yourData) ? <div className='lbHeader'>Your Rank</div> : null}
 				{(role === "volunteer" && yourData) ? <YourRank/> : null}
-				{(role === "volunteer" && studentData) ? <div className='lbHeader'>Top 10</div> : null}
+				{(role === "organization" && studentData) ? <Search studentData={studentData} searchID={searchID} setSearchID={setSearchID}/> : null}
+				{(role === "organization" && searchID) ? <SearchRank/> : null}
+				{(studentData) ? <div className='lbHeader'>Top 10</div> : null}
 			  	{(studentData) ? studentData.slice(0, 10).map((student, i) => <RankCard student={student[0]} pic={student[1]} i={i + 1}/>) : null}
 			  </div>
 		  </div>
