5a928b2a42e6b10ef8b22cca44d05893c3e3a28e
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-02-10 11:03:22 -0500

    update the documentation for how the storing to S3 API works and also adding fields to schemas to store image credentials + some security approaches added

--- backend/routes/image_processing/newStoreImage_S3.js
+++ backend/routes/image_processing/newStoreImage_S3.js
@@ -43,6 +43,14 @@ const randomImageName = (bytes = 32) => {
 
 router.post('/', upload.single('profilePic'), async (req, res) => {
 
+    // [IMPORTANT !!]
+    // event picture: typeOfImage = 1
+    // organization profilepicture: typeOfImage = 2
+    // user profile picture: typeOfImage = 3
+    // organization background picture: typeOfImage = 4
+
+    const typeOfImage = req.body.typeOfImage;
+
     console.log('req.body: ', req.body);
     console.log('req.file: ', req.file);
 
@@ -51,9 +59,10 @@ router.post('/', upload.single('profilePic'), async (req, res) => {
         .resize({heigh: 200, width: 200, fit:"contain"})
         .toBuffer(); // [feel free to modify the dimensions within the .resize()]
 
+    const imageName = randomImageName();
     const params = {
         Bucket: S3_BUCKET_NAME,
-        Key: randomImageName(), // doing this so it doesn't overwrite the existing images if they have the same name 
+        Key: imageName, // doing this so it doesn't overwrite the existing images if they have the same name 
         Body: resizedImage,
         ContentType: req.file.mimetype,
     };
@@ -68,7 +77,21 @@ router.post('/', upload.single('profilePic'), async (req, res) => {
         res.status(500).send('Failed to upload to S3');
     }
 
-    res.send({});
+    if (typeOfImage === '1') {
+        await Event.findByIdAndUpdate(req.body.id, { S3BucketImageDetails: imageName });
+    } else if (typeOfImage === '2') {
+        await Organization.findByIdAndUpdate(req.body.id, { S3BucketImageDetails_ProfilePic: imageName });
+    } else if (typeOfImage === '3') {
+        await UserStudent.findByIdAndUpdate(req.body.id, { S3BucketImageDetails: imageName });
+    } else if (typeOfImage === '4') {
+        await Organization.findByIdAndUpdate(req.body.id, { S3BucketImageDetails_Background: imageName });
+    }
+
+    console.log('Image name: ', imageName);
+    console.log('Image type: ', typeOfImage);
+    console.log("image stored for the entity with id: ", req.body.id);
+
+    res.status(200).send('Image stored in S3 and the image name stored in the database');
 
 });
 
