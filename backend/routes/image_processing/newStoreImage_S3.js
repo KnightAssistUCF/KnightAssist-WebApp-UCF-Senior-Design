d28f337d0f42840dac48eef9abae65d1927c340e
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-02-10 10:36:39 -0500

    added aws dependencies and also created the initial version of the storing images using S3 (no account setup on aws waiting on PM and not tested yet, and no .env yet)

---
+++ backend/routes/image_processing/newStoreImage_S3.js
@@ -0,0 +1,63 @@
+/* Create a new image in S3 and store it */
+
+/* Everytime we upload a new file to S3 bucket that has an existing name as a file within S3, 
+then the one in S3 is overwritten with the new one */
+
+const express = require('express');
+const multer = require('multer');
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+require('dotenv').config();
+import { S3CLIENT, PutObjectCommand } from "@aws-sdk/client-s3"; // what we use to interact with the S3 client
+// declaring variables to store the S3 bucket name and the region, access ky and secret access key from the .env file
+const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const S3_REGION = process.env.S3_REGION;
+const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY;
+const S3_SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY;
+
+const S3 = new S3CLIENT({
+    region: S3_REGION,
+    credentials: {
+        accessKeyId: S3_ACCESS_KEY,
+        secretAccessKey: S3_SECRET_ACCESS_KEY
+    }
+});
+
+const Event = require('../../models/events');
+const Organization = require('../../models/organization');
+const UserStudent = require('../../models/userStudent');
+
+const upload = multer({ storage: storage });
+const storage = multer.memoryStorage();
+
+
+router.post('/', upload.single('profilePic'), async (req, res) => {
+
+    console.log('req.body: ', req.body);
+    console.log('req.file: ', req.file);
+    req.file.buffer
+
+    const params = {
+        Bucket: S3_BUCKET_NAME,
+        Key: req.file.originalname, // whatever the file name was on the user's computer before they uploaded it 
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype,
+    };
+
+    const command = new PutObjectCommand(params);
+
+    try {
+        await S3.send(command);
+        res.status(200).send('Uploaded to S3 the image names + ' + req.file.originalname);
+    } catch (err) {
+        console.log('Error: ', err);
+        res.status(500).send('Failed to upload to S3');
+    }
+
+    res.send({});
+
+});
+
+module.exports = router;
