33ad80900fc61b68c9f7c49f4ac710f0624db070
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-02-10 11:42:12 -0500

    getting the setup for the delete endpoint setup, only boiler plate stuff for now, will be added soon

--- backend/routes/image_processing/newDeleteImage_S3.js
+++ backend/routes/image_processing/newDeleteImage_S3.js
@@ -39,50 +39,12 @@ const upload = multer({ storage: storage });
 // specific presigned imorts and global declarations
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-router.get('/', async (req, res) => {
+router.delete('/', async (req, res) => {
 
     // event picture: typeOfImage = 1
     // organization profilepicture: typeOfImage = 2
     // user profile picture: typeOfImage = 3
     // organization background picture: typeOfImage = 4
-    const typeOfImage = req.body.typeOfImage;
-
-    // get all the image S3 bucket names and store them in a list
-    const S3_imageNames = [];
-    if (typeOfImage === 1) {
-        const events = await Event.find();
-        events.forEach(event => {
-            S3_imageNames.push(event.S3BucketImageDetails);
-        });
-    } else if (typeOfImage === 2) {
-        const organizations = await Organization.find();
-        organizations.forEach(organization => {
-            S3_imageNames.push(organization.S3BucketImageDetails_ProfilePic);
-        });
-    } else if (typeOfImage === 3) {
-        const users = await UserStudent.find();
-        users.forEach(user => {
-            S3_imageNames.push(user.S3BucketImageDetails);
-        });
-    } else if (typeOfImage === 4) {
-        const organizations = await Organization.find();
-        organizations.forEach(organization => {
-            S3_imageNames.push(organization.S3BucketImageDetails_Background);
-        });
-    }
-
-    for(const imageName of S3_imageNames) {
-        const getObjectParams = {
-            Bucket: S3_BUCKET_NAME,
-            Key: imageName.imageName
-        };
-        const command = new GetObjectCommand({ getObjectParams }); // to creat the URL
-        const url = await getSignedUrl(S3, command, { expiresIn: 10000 }); // temporrary access to the image, to renew the user can make a new access to the website or just call this endpoint
-        console.log('url: ', url);
-        imageName.url = url;
-    }
-
-    res.status(200).send("Retrieved the image names and their URLs from S3 bucket after generation");
 
 });
 
