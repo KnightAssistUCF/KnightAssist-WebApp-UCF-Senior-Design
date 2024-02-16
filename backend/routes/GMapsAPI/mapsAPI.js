06729459f59df8adf803387dc086d8c85240dc69
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-02-16 13:53:04 -0500

    attempting to fix issue with fetch node

--- backend/routes/GMapsAPI/mapsAPI.js
+++ backend/routes/GMapsAPI/mapsAPI.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const fetch = (await import('node-fetch')).default;
 require('dotenv').config();
 const router = express.Router();
 
