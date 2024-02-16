6508f49195642dcc3638dbd029531fc312dc7dc6
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-02-16 13:22:02 -0500

    creating the initial version of the google maps api incorporated to find the address passed in as a parameter

---
+++ backend/routes/GMapsAPI/mapsAPI.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fetch = require('node-fetch');
+require('dotenv').config();
+const router = express.Router();
+
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
+router.get('/', async (req, res) => {
+    const { address } = req.body;
+    if (!address) {
+        return res.status(400).send({ error: 'Address for using the google maps API is required' });
+    }
+
+    const geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${GOOGLE_MAPS_API_KEY}`;
+
+    try {
+        const response = await fetch(geocodeURL);
+        const data = await response.json();
+        if (data.status !== 'OK') {
+            return res.status(404).send({ error: 'Location not found through the google map api nor the key' });
+        }
+
+        const { location } = data.results[0].geometry;
+        res.send(location); // we send back the latitude and longitude as a result to the front end
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: 'Failed to fetch location' });
+    }
+});
+
+module.exports = router;
