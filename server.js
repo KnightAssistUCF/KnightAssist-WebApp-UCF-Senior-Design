eff94a5b6bf9eeb3f93b6bf21b3b04187fcfb074
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-02-20 21:23:20 -0500

    fixing server.js with the new endpoint

--- server.js
+++ server.js
@@ -300,6 +300,10 @@ app.use('/api/deleteImage', deleteImage_S3);
 const mapsAPI = require('./backend/routes/GMapsAPI/mapsAPI');
 app.use('/api/mapsAPI', mapsAPI);
 
+/* Push notification every 30 min */
+const pushNotifications_30min = require('./backend/routes/PushNotifications/pushNotifications_30min');
+app.use('/api/pushNotifications_30min', pushNotifications_30min);
+
 /*
   if we plan to have specific settings for the configuration in production, we will need to add that here.
   can be omitted for now
