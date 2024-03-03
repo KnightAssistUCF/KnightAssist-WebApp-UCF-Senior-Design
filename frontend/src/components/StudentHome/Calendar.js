4995e8a5881dbb729116c12fa9d75ce33fbf67ef
AnishaRan <anisharanjan55@gmail.com>
2024-03-03 00:28:21 -0500

    styling

--- frontend/src/components/StudentHome/Calendar.js
+++ frontend/src/components/StudentHome/Calendar.js
@@ -6,36 +6,50 @@ import Snackbar from '@mui/joy/Snackbar';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 
-function Calendar() {
+function Calendar({upcomingRSVPdEvents}) {
 //   const [upcomingEvents, setUpcomingEvents] = useState([]);
 //   const [rsvpEvents, setRSVPEvents] = useState([]);
 //   const [message, setMessage] = useState("");
 //   const [open, setOpen] = useState(false);
 
-const [upcomingEvents, setUpcomingEvents] = useState([]);
+const [RSVPdEvents, setRSVPdEvents] = useState([]);
 
 async function getStudentInfo() {
-    upcomingEvents.push({
-        _id: '1',
-        title: 'Name',
-        start: new Date(),
-        end: new Date(),
-        editable: false,
-        deletable: false,
-        draggable: false,
-        description: 'fsdlf',
-        location: 'sdfd',
-        maxAttendees: 5,
-        numRegistered: 9,
-        rsvpStatus: false
-    });
+    const upcomingEvents = [];
+    console.log("hereeee");
+    if(upcomingRSVPdEvents.length > 0) {
+        for(const event of upcomingRSVPdEvents) {
+            console.log(event._id);
+            console.log(event.startTime);
+            console.log(new Date(event.startTime));
+            upcomingEvents.push({
+                _id: event._id,
+                title: event.name,
+                start: new Date(event.startTime),
+                end: new Date(event.endTime),
+                editable: false,
+                deletable: false,
+                draggable: false,
+                description: 'fsdlf',
+                location: 'sdfd',
+                maxAttendees: 5,
+                numRegistered: 9,
+                rsvpStatus: "Undo RSVP"
+            });
+            console.log(upcomingEvents)
+        }
+        setRSVPdEvents(upcomingEvents);
+    } else {
+        console.log("Empty RSVP array");
+    }
+
 }
 
 
 useEffect(() => {
     getStudentInfo();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [upcomingRSVPdEvents]);
 
 // _id: event._id,
 // title: event.name,
@@ -80,82 +94,47 @@ const customViewer = (event, close) => {
   
     async function unrsvpEvent(event){
       try {
-        // var url = buildPath(`api/cancelRSVP`);
-        // var studentID = sessionStorage.getItem("ID");
-        // var json = {
-        //   eventID: event._id,
-        //   eventName: event.title,
-        //   userID: studentID
-        // }
-        // console.log(json);
-        // var response = await fetch(url, {
-        //   body: JSON.stringify(json),
-        //   method: "DELETE",
-        //   headers: {"Content-Type": "application/json"},
-        // });
+        var url = buildPath(`api/cancelRSVP`);
+        var studentID = sessionStorage.getItem("ID");
+        var json = {
+          eventID: event._id,
+          eventName: event.title,
+          userID: studentID
+        }
+        console.log(json);
+        var response = await fetch(url, {
+          body: JSON.stringify(json),
+          method: "DELETE",
+          headers: {"Content-Type": "application/json"},
+        });
         
-        // var res = await response.text();
-        // console.log(res);
-        // setRSVPEvents(prevRSVPEvents => prevRSVPEvents.filter(e => e._id !== event._id));
+        var res = await response.text();
+        console.log(res);
+        setRSVPdEvents(RSVPdEvents => RSVPdEvents.filter(e => e._id !== event._id));
       } catch(e) {
         console.log("unrsvp failed");
       }
     }
   
-    async function rsvpEvent(event){
-      console.log(event);
-      try {
-        // var url = buildPath(`api/RSVPForEvent`);
-  
-        // var studentID = sessionStorage.getItem("ID");
-        // console.log("rsvp Event try");
-        // var json = {
-        //   eventID: event._id,
-        //   eventName: event.title,
-        //   userID: studentID,
-        //   check: 0
-        // }
-        // console.log(json);
-        // var response = await fetch(url, {
-        //   body: JSON.stringify(json),
-        //   method: "POST",
-        //   headers: {"Content-Type": "application/json"},
-        // });
-        
-        // var res = await response.text();
-  
-        // console.log(res);
-        // setRSVPEvents(prevRSVPEvents => [...prevRSVPEvents, event]);
-      } catch(e) {
-        console.log("rsvp API call failed: " + e);
-      }
-    }
   
     const handleButtonClick = (event) => {
-    //   if (event.rsvpStatus === "Full Capacity") {
-    //     console.log("Full");
-    //   } else if (event.rsvpStatus === "RSVP") {
-    //     rsvpEvent(event);
-    //     setMessage("RSVP Successful")
-    //     setOpen(true);
-    //   } else if (event.rsvpStatus === "Undo RSVP") {
-    //     unrsvpEvent(event);
-    //     setMessage("Cancelled RSVP")
-    //     setOpen(true);
-    //   }
+        if (event.rsvpStatus === "Undo RSVP") {
+            unrsvpEvent(event);
+            // setMessage("Cancelled RSVP")
+            // setOpen(true);
+        }
   
       close();
     };
+
     return (
       <Box sx={{maxWidth: '500px'}}>
         <Box sx={{ margin: "20px" }}>
           <div>
-          <p>{`${event._id}`}</p>
             <h3>{event.title}</h3>
             <p>{formatDateTimeRange(event.start, event.end)}</p>
             <p style={{ margin: '0', lineHeight: '0.7' }}>{"Capacity: " + event.numRegistered + '/' + event.maxAttendees}</p>
             <p style={{marginTop: '8px'}}>{"Location: "+event.location}</p>
-            <p>{`${event.rsvpStatus}`}</p>
             <p>{`${event.description}`}</p>
           </div>
           <div>
@@ -179,10 +158,10 @@ const customViewer = (event, close) => {
     <div style={{color: 'black', marginBottom: '20px'}}>
         <Scheduler
           view="month"
-          height={450}
+          height={520}
           //width={400}
           editable={false}
-          events={upcomingEvents}
+          events={RSVPdEvents}
           customViewer={customViewer}
         />
     </div>
