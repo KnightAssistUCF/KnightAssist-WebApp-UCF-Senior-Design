96df97a449d9060ef777b8bf48391fc2110d2948
Alyssa <yeekeea@gmail.com>
2024-03-07 19:44:02 -0500

    fixing icon + text issues in the details section

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,60 +1,32 @@
 import * as React from 'react';
-import { Grid, Typography, Button, Box, Link} from '@mui/material';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import { Grid, Typography, Box } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import PreLoginNavBar from '../../PreLogin/PreLoginNavBar';
-import useStyles from '../../PreLogin/PreLoginStyles';
 import './Contact.css';
 import UserInput from './UserInput';
 import Details from './Details';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Footer from '../Footer';
-import Paper from '@mui/material/Paper';
-import Container from '@mui/material/Container';
 
 const theme = createTheme();
 
 function Contact() {
-  const classes = useStyles();
-
-  // Make a custom footer based on 
-  // the existing one for styling purposes
-  {/*function Footer(){
-      return (
-		<div className='contactFooter'>
-			{"Copyright © "}
-			<Link color="inherit" href="https://knightassist-43ab3aeaada9.herokuapp.com/">
-				KnightAssist
-			</Link>{" "}
-			{new Date().getFullYear()}
-			{"."}
-		</div>
-	  )
-  }*/}
-
   return (
 	<ThemeProvider theme={theme}>
 		<CssBaseline />
 		<Box sx={{ background: 'linear-gradient(to right, #F5FAF9, #E4D9FB)', minHeight: '100%', flexGrow: 1, padding: 10, justifyContent: 'center', alignItems: 'center'}}>
 			<PreLoginNavBar />
-			<Box sx={{ marginTop: 10, marginRight: '15%', marginLeft: '15%', display: 'flex', flexDirection: 'column' }} >
+			<Box sx={{ marginTop: 10, marginRight: '12%', marginLeft: '12%', display: 'flex', flexDirection: 'column' }} >
 				<Typography component="h1" variant="h3" align="left" fontWeight={700} sx={{ marginTop: 5, color: '#212121' }}>
 					Contact Us
 				</Typography>
 				<Grid container component="main" sx={{ flex: 1, display: 'flex', flexdirection: 'column' }}>
-					<Grid Item
-						xs={false}
-						sm={4}
-						md={6}
-						flexDirection="column"
-						align="left"
-					>
+					<Grid Item xs={false} sm={4} md={6} flexDirection="column" align="left">
 						<Typography component="h1" variant="h4" sx={{ marginTop: 5, marginRight: 10, color: '#00522B' }}>
 							Got questions? We are here to help.
 						</Typography>
 						<Typography component="h6" variant="h6" sx={{ marginTop: 5, marginBottom: 5, marginRight: 10, color: '#212121' }}>
-							If you need to reach our team with inquires, pleaase fill out the form below with any questions or concerns, and our team will get back to you as soon as possible!
+							If you need to reach our team with inquires, please fill out the form below with any questions or concerns, and our team will get back to you as soon as possible!
 						</Typography>
 						<Details />
 					</Grid>
