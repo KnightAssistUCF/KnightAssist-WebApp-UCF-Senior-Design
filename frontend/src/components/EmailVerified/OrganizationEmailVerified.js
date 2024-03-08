97845540ccf3765aa618eba297a9eb177805835a
AnishaRan <anisharanjan55@gmail.com>
2024-03-07 20:06:50 -0500

    resolving breakpoint error on student and org email verified pages

--- frontend/src/components/EmailVerified/OrganizationEmailVerified.js
+++ frontend/src/components/EmailVerified/OrganizationEmailVerified.js
@@ -7,11 +7,18 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { buildPath } from "../../path";
 import PreLoginNavBar from '../../PreLogin/PreLoginNavBar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import useStyles from '../../PreLogin/PreLoginStyles';
+
+const theme = createTheme();
 
 function OrganizationEmailVerified() {
   const [verificationCode, setVerificationCode] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+
+  const classes = useStyles();
 
 
   async function submitVerifyCode() {
@@ -52,21 +59,24 @@ function OrganizationEmailVerified() {
 
 return (
         <div id='emailVerified'>
-          <PreLoginNavBar />
-          <div className="CardContainer">
-            <Card variant="outlined" className="Card" style={{backgroundColor: "rgba(169, 169, 169, 0.05)"}}>
-              <Box sx={{ margin: '20px' }}>
-                <div className="emailContent">Thanks for signing up! Please enter the confirmation code from your email to verify your account.</div>
-                <BiMailSend  className='verifyIcon'></BiMailSend >
-              </Box>
-              <TextField id="outlined-basic" label="Organization Email" variant="outlined" value={email} onChange={(event) => setEmail(event.target.value)} style={{ marginBottom: '10px'}} />
-              <TextField id="outlined-basic" label="Verification Code" variant="outlined" value={verificationCode} onChange={(event) => setVerificationCode(event.target.value)} />
-              <Button className="submitBtn" variant="contained" color="primary" style={{ backgroundColor: '#5B4E77'}} onClick={submitVerifyCode}>
-                Submit
-              </Button>
-              <div style={{ color: message === "Correct code" ? "green" : "red", fontSize: '20px' }}>{message}</div>
-            </Card>
-          </div>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <PreLoginNavBar />
+            <div className="CardContainer">
+              <Card variant="outlined" className="Card" style={{backgroundColor: "rgba(169, 169, 169, 0.05)"}}>
+                <Box sx={{ margin: '20px' }}>
+                  <div className="emailContent">Thanks for signing up! Please enter the confirmation code from your email to verify your account.</div>
+                  <BiMailSend  className='verifyIcon'></BiMailSend >
+                </Box>
+                <TextField id="outlined-basic" label="Organization Email" variant="outlined" value={email} onChange={(event) => setEmail(event.target.value)} style={{ marginBottom: '10px'}} />
+                <TextField id="outlined-basic" label="Verification Code" variant="outlined" value={verificationCode} onChange={(event) => setVerificationCode(event.target.value)} />
+                <Button className="submitBtn" variant="contained" color="primary" style={{ backgroundColor: '#5B4E77'}} onClick={submitVerifyCode}>
+                  Submit
+                </Button>
+                <div style={{ color: message === "Correct code" ? "green" : "red", fontSize: '20px' }}>{message}</div>
+              </Card>
+            </div>
+          </ThemeProvider>
         </div>
       );
     }
