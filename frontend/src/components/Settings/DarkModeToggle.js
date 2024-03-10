3490253fae2d0131d3285cee56059d59a00e1b1c
Melanie Ehrlich <melanie6104@icloud.com>
2024-03-09 23:24:33 -0500

    add sessionstorage

--- frontend/src/components/Settings/DarkModeToggle.js
+++ frontend/src/components/Settings/DarkModeToggle.js
@@ -1,11 +1,30 @@
-import {React, useState} from "react";
+import {React, useState, useEffect} from "react";
 import "./toggle.css";
 
 export const DarkModeToggle = () => {
     const [toggle, setToggle] = useState(false);
+
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const themeData = defaultDark ? 'dark' : 'light';
+const [theme, setTheme] = useState(sessionStorage.getItem("theme"));
+sessionStorage.setItem("theme", themeData);
+
+    useEffect(() => {
+        if (toggle) {
+          document.body.classList.add('dark');
+        } else {
+          document.body.classList.remove('dark');
+        }
+        switchTheme();
+      },); 
+
+      const switchTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+      }
   return (
     <>
-        <div className="t1">Turn {toggle ? 'off' : 'on'} the dark mode</div>
+        <div className="t1" data-theme={theme}>Turn {toggle ? 'off' : 'on'} the dark mode</div>
        
         <div className='toggle-container' onClick={() =>  setToggle({
                 toggle: !toggle
