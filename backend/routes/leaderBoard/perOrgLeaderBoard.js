cd4d4e8bcdfa50c34479dad40da18d0d4b3c4792
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2024-03-14 08:08:28 -0400

    updated the per org leaderboard to have an output format similar to the gloabl leaderboard one

--- backend/routes/leaderBoard/perOrgLeaderBoard.js
+++ backend/routes/leaderBoard/perOrgLeaderBoard.js
@@ -13,23 +13,27 @@ router.get('/', async (req, res) => {
         }
 
         // Fetch all students who have volunteered for the organization
-        const students = await UserStudent.find({ 'hoursPerOrg': { $exists: true } }).select('firstName lastName hoursPerOrg');
+        const students = await UserStudent.find({ 'hoursPerOrg': { $exists: true } })
+            .select('firstName lastName hoursPerOrg totalVolunteerHours');
 
-        let volunteerDetails = students.map(student => {
-            const totalHours = student.hoursPerOrg.get(orgId) || 0;
-            return {
-                student: {
+        let volunteerDetails = students.filter(student => student.hoursPerOrg.get(orgId))
+            .map(student => {
+                return {
+                    _id: student._id,
                     firstName: student.firstName,
-                    lastName: student.lastName
-                },
-                hoursVolunteered: totalHours
-            };
-        });
+                    lastName: student.lastName,
+                    totalVolunteerHours: student.hoursPerOrg.get(orgId),
+                };
+            });
 
         // Sort the array based on hours volunteered in descending order
-        volunteerDetails.sort((a, b) => b.hoursVolunteered - a.hoursVolunteered);
+        volunteerDetails.sort((a, b) => b.totalVolunteerHours - a.totalVolunteerHours);
 
-        res.json({ data: volunteerDetails });
+        res.json({
+            success: true,
+            message: 'Students ranked by total volunteer hours for the organization',
+            data: volunteerDetails
+        });
     } catch (error) {
         console.error('Internal error: ', error);
         res.status(500).send('Internal server error while making the per org leaderboard.');
