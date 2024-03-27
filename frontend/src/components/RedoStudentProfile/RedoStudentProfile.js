71545f9b4ee6662589540d808542f062e4a748ef
NSel1727 <noahseligson18@gmail.com>
2024-03-27 18:48:12 -0400

    Fix bug with viewing student profile as org

--- frontend/src/components/RedoStudentProfile/RedoStudentProfile.js
+++ frontend/src/components/RedoStudentProfile/RedoStudentProfile.js
@@ -4,6 +4,8 @@ import './RedoStudentProfile.css';
 import StudentHeader from '../StudentHome/StudentHeader';
 import StudentTopBar from '../TopBar/StudentTopBar';
 import StudentProfileTabs from './StudentProfileTabs';
+import Header from '../OrgEvents/Header';
+import OrgTopBar from '../OrgHome/OrgTopBar';
 
 
 function RedoStudentProfile()
@@ -12,17 +14,11 @@ function RedoStudentProfile()
     return(
         <div className='fontFamily'>
             <div className='redoStudentProfile'>
-            <StudentHeader/>
-            <StudentTopBar title="Profile"/>
-            <div className="redoStudentProfileContent">
-                
-                <StudentProfileTabs/>
-
-
-
-
-
-            </div>
+				{(sessionStorage.getItem("role") === "volunteer") ? <StudentHeader/> : <Header/>}
+				{(sessionStorage.getItem("role") === "volunteer") ? <StudentTopBar title="Profile"/> : <OrgTopBar title="Profile"/>}
+				<div className="redoStudentProfileContent">
+					<StudentProfileTabs/>
+				</div>
             </div>
         </div>
     );
